4139349ab6f27077757ec0c4c0f487e28833caca
Nick Strayer <nick.strayer@gmail.com>
2019-07-17 21:14:43 +0000

    Fixed remove node to use pointers properly and added get_node_by_id method.

--- BiGraph.cpp
+++ BiGraph.cpp
@@ -39,21 +39,55 @@ void BiGraph::add_node(string name, bool type_a){
 //=================================
 void BiGraph::remove_node(string name, bool type_a){
   
-  map<string, Node>            map_for_type = type_a ? a_nodes : b_nodes; // Node map for type
-  map<string, Node>::iterator  node_to_delete;                            // Itterator to loop through node map
+  // Pointer to the correct map for type
+  NodeMap*            map_for_type_ptr = type_a ? &a_nodes : &b_nodes;
+  NodeMap::iterator   node_to_delete;                                 // Itterator to loop through node map
   
   // Try and find the node.
-  node_to_delete = map_for_type.find(name);
+  node_to_delete = map_for_type_ptr->find(name);
   
   // Is there already a connection to this node?
-  bool node_exists = node_to_delete != map_for_type.end();
+  bool node_exists = node_to_delete != map_for_type_ptr->end();
   
   // If node exists, remove it.
   if(node_exists){
-    map_for_type.erase(node_to_delete);
+    map_for_type_ptr->erase(node_to_delete);
+  }
+}
+
+
+//=================================
+// Get pointer to node 
+// If node doesn't exist it is created
+//=================================
+Node* BiGraph::get_node_by_id(string name, bool type_a){
+  NodeMap*            map_for_type_ptr;   // Pointer to the correct map for type
+  NodeMap::iterator   node_to_find;       // Iterator for finding node
+  Node *              node_to_return_ptr; // Pointer for node requested
+  // Grab correct node map
+  map_for_type_ptr = type_a ? &a_nodes : &b_nodes;
+  
+  // Try and find the node.
+  node_to_find = map_for_type_ptr->find(name);
+  
+  // Is there already a connection to this node?
+  bool node_doesnt_exist = node_to_find == map_for_type_ptr->end();
+  
+  if(node_doesnt_exist){
+    // Insert node into map and assign newly inserted value
+    // to our node finding iterator.
+    node_to_find = map_for_type_ptr->insert(
+        std::pair<string, Node>(
+            name, 
+            Node(name, type_a)
+        )
+    ).first; // .first is the the equivalent of map->find(...)'s results
   }
   
+  // Return a pointer to the node
+  return &node_to_find->second;
 }
+
 
 
 
@@ -61,8 +95,8 @@ void BiGraph::remove_node(string name, bool type_a){
 // Methods to declare
 //=================================
 
+
 // void                                  BiGraph::add_edge(string, string);
-// Node*                                 BiGraph::get_node_by_id(string, bool);
 // Node*                                 BiGraph::draw_random_node(string, bool);
 // void                                  BiGraph::cleanup_empty_clusters();       // Removes nodes with no members
 // vector< tuple<string, string> >       BiGraph::take_cluster_snapshot();
@@ -71,12 +105,18 @@ void BiGraph::remove_node(string name, bool type_a){
 
 
 // [[Rcpp::export]]
-List load_data(vector<string> edges_a, vector<string> edges_b){
+List load_data(vector<string> edges_a, vector<string> edges_b, bool delete_a_node){
   BiGraph my_bigraph;
   
-  my_bigraph.add_node("node_1", true);
+  my_bigraph.get_node_by_id("node_1", true);
   my_bigraph.add_node("node_2", true);
   my_bigraph.add_node("node_3", false);
+  my_bigraph.get_node_by_id("node_1", true);
+  
+  
+  if(delete_a_node){
+    my_bigraph.remove_node("node_1", true);
+  }
   
   return List::create(
     _["n_nodes"]                 = my_bigraph.a_nodes.size()
@@ -87,6 +127,6 @@ List load_data(vector<string> edges_a, vector<string> edges_b){
 
 /*** R
 data <- readr::read_csv('southern_women.csv', col_types = readr::cols(event = 'c', individual = 'c')) 
-load_data(data$event, data$individual)
-
+load_data(data$event, data$individual, FALSE)
+load_data(data$event, data$individual, TRUE)
 */
