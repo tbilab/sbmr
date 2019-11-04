7196c7d5bb17c3bafee4f06037f2650006ee2f37
Nick Strayer <nick.strayer@gmail.com>
2019-11-04 13:39:32 -0600

    Made name of get_nodes_of_type more verbose to get_nodes_of_type_at_level() and also fixed bug that always caused it to look at level 0 (aka a hardcoded 0).

--- SBM.cpp
+++ SBM.cpp
@@ -1,10 +1,13 @@
-#include <Rcpp.h>
-#include <random>
+#include <queue> 
+#include <unordered_set>
+#include <string>
+#include <vector>
+
 #include "Node.h" 
 #include "SBM.h" 
+#include "Node.cpp"
 #include "helpers.cpp"
 
-using namespace Rcpp;
 using std::string;
 using std::vector;
 using std::map;
@@ -88,13 +91,13 @@ Node* SBM::get_node_by_id(string desired_id, int node_type){
 // =======================================================
 // Return nodes of a desired type from level
 // =======================================================
-NodeList SBM::get_nodes_of_type(int type, int level) {
+NodeList SBM::get_nodes_of_type_at_level(int type, int level) {
   NodeList            nodes_to_return;
   NodeList::iterator  node_it;
   NodeList*           node_level;
   
   // Grab desired level reference
-  node_level = get_node_level(0);
+  node_level = get_node_level(level);
   
   // Loop through every node belonging to the desired level
   for (node_it = node_level->begin(); node_it != node_level->end(); ++node_it) {
@@ -114,21 +117,24 @@ NodeList SBM::get_nodes_of_type(int type, int level) {
 // =======================================================
 // Creates a new group node and adds it to its neccesary level
 // =======================================================
-Node* SBM::create_group_node(int level, int type) {
+Node* SBM::create_group_node(int type, int level) {
   
   NodeList*  group_level;
-  int        n_groups_at_level;
+  int        n_groups_in_level;
   string     group_id;
   Node*      new_group;
+  
+  // Make sure requested level is not 0
+  if(level == 0) throw "Can't create group node at first level";
   
   // Grab level for group node
   group_level = get_node_level(level);
   
-  // Find how many groups are in the current level
-  n_groups_at_level = group_level->size();
+  // Find how many groups are already in the current level (all types)
+  n_groups_in_level = group_level->size();
   
   // Build group_id
-  group_id = std::to_string(type) + "-" + std::to_string(level) + "_" + std::to_string(n_groups_at_level);
+  group_id = std::to_string(type) + "-" + std::to_string(level) + "_" + std::to_string(n_groups_in_level);
   
   // Initialize new node
   new_group = new Node(group_id, level, type);
@@ -139,56 +145,3 @@ Node* SBM::create_group_node(int level, int type) {
   return new_group;
 };
 
-
-// [[Rcpp::export]]
-List load_data(vector<string> edges_a, vector<string> edges_b){
-  SBM my_SBM;
-  int n_edges = edges_a.size();
-  
-  return List::create(
-    _["n_edges"] = n_edges
-  );
-}
-
-
-
-
-// =======================================================
-
-// =======================================================
-
-
-// [[Rcpp::export]]
-List setup_SBM(){
-  SBM my_SBM;
-  
-  // Add some nodes to SBM
-  my_SBM.get_node_by_id("n1", 0);
-  my_SBM.get_node_by_id("n2", 0);
-  my_SBM.get_node_by_id("n3", 0);
-  my_SBM.get_node_by_id("n3", 0); // Duplicate node
-  my_SBM.get_node_by_id("m1", 1);
-  my_SBM.get_node_by_id("m2", 1);
-  my_SBM.get_node_by_id("m3", 1);
-  my_SBM.get_node_by_id("m4", 1);
-  
-  // Create a group node
-  my_SBM.create_group_node(1, 0);
-  my_SBM.create_group_node(1, 1);
-  
-  return List::create(
-    _["num_nodes"]            = my_SBM.nodes[0].size(),
-    _["level 0"]              = print_node_ids(*my_SBM.get_node_level(0)),
-    _["level 1"]              = print_node_ids(*my_SBM.get_node_level(1)),
-    _["nodes of first type"]  = print_node_ids(my_SBM.get_nodes_of_type(0,0)),
-    _["nodes of second type"] = print_node_ids(my_SBM.get_nodes_of_type(1,0)),
-    _["num levels"]           = my_SBM.nodes.size()
-  );
-}
-
-/*** R
-# data <- readr::read_csv('southern_women.csv', col_types = readr::cols(event = 'c', individual = 'c'))
-# load_data(data$event, data$individual)
-
-setup_SBM()
-*/
--- SBM.h
+++ SBM.h
@@ -6,9 +6,6 @@
 #define __SBM_INCLUDED__
 
 #include "Node.h" 
-//using std::string;
-//using std::tuple;
-//using std::vector;
 using std::map;
 
 typedef vector<Node*> NodeList;
@@ -34,7 +31,7 @@ class SBM {
     // ==========================================
     // Methods
     NodeList*  get_node_level(int);            // Grabs reference to nodes at desired level, if level doesn't exist, it makes it
-    NodeList   get_nodes_of_type(int, int);    // Return nodes of a desired type from level
+    NodeList   get_nodes_of_type_at_level(int, int);    // Return nodes of a desired type from level
     Node*      create_group_node(int, int);    // Creates a new group node and adds it to its neccesary level
     int        clean_empty_groups();           // Parses through network and removes all empty groups from network, returns num culled
     Node*      get_node_by_id(string, int);    // Grabs and returns node of specified id, if node doesn't exist, node is created first
