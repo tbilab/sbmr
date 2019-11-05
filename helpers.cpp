d361be4bdc1b26fdafbc571bab7772185362ff41
Nick Strayer <nick.strayer@gmail.com>
2019-11-05 11:46:01 -0600

    Added sorting to the print nodes helper function so I order of returned ids doesn't matter.

--- helpers.cpp
+++ helpers.cpp
@@ -1,22 +1,36 @@
 #include <string>
+#include <algorithm>    // std::sort
+#include <vector>
 
 // =======================================================
 // Print vector of node ids, for debugging
+// Sorts contents so we can test non-order guarenteed containers
 // =======================================================
 template <class container> 
 std::string print_node_ids(container nodes) {
   // Template works for iteratable types like vector and sets
-  std::string                  node_ids;
+  std::vector<std::string>           node_ids;
+  std::vector<std::string>::iterator node_id_it;
+  std::string                        node_id_string;
   typename container::iterator node_it;
   
+  // Add node ids to containing vector
   for (node_it = nodes.begin(); node_it != nodes.end(); ++node_it) {
     // Append node id to return string.
-    node_ids.append((*node_it)->id + ", ");
+    node_ids.push_back((*node_it)->id);
+  }
+  
+  // Sort vector of id strings
+  std::sort(node_ids.begin(), node_ids.end());
+
+  // Dump vector of id strings to one big string
+  for (node_id_it = node_ids.begin(); node_id_it != node_ids.end(); ++node_id_it) {
+    // Append node id to return string.
+    node_id_string.append(*node_id_it + ", ");
   }
   
   // Remove last comma for cleanliness
-  node_ids.erase(node_ids.end() - 2, node_ids.end());
+  node_id_string.erase(node_id_string.end() - 2, node_id_string.end());
   
-  return node_ids;
+  return node_id_string;
 }
-
