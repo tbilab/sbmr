98e40fcdca96be4a0207174b4d03881eb1df24d0
Nick Strayer <nick.strayer@gmail.com>
2019-11-08 16:52:28 -0600

    Added reference to the Weighted_Sampler class in the test compilation for SBM tests.

--- cpp_tests/run_tests.sh
+++ cpp_tests/run_tests.sh
@@ -9,21 +9,21 @@ g++ -std=c++11 -c Node.cpp SBM.cpp helpers.cpp Weighted_Sampler.cpp
 # g++ -std=c++11 -c Node.cpp helpers.cpp
 
 
-# echo "========================================\nTesting Node.cpp"
-# g++ -std=c++11 \
-#   -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
-#   -pthread cpp_tests/test_node.cpp Node.o helpers.o\
-#   /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
-# ./my_googletest
-# 
-# 
-# echo "========================================\nTesting SBM.cpp"
-# g++ -std=c++11 \
-#   -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
-#   -pthread cpp_tests/test_sbm.cpp Node.o SBM.o helpers.o\
-#   /home/nick/cpp_dev/gtest_build/lib/libgtest.a \
-#   -o my_googletest
-# ./my_googletest
+echo "========================================\nTesting Node.cpp"
+g++ -std=c++11 \
+  -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
+  -pthread cpp_tests/test_node.cpp Node.o helpers.o\
+  /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
+./my_googletest
+
+
+echo "========================================\nTesting SBM.cpp"
+g++ -std=c++11 \
+  -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
+  -pthread cpp_tests/test_sbm.cpp Node.o SBM.o helpers.o Weighted_Sampler.o\
+  /home/nick/cpp_dev/gtest_build/lib/libgtest.a \
+  -o my_googletest
+./my_googletest
 
 
 echo "========================================\nTesting Weighted_Sampler.cpp"
