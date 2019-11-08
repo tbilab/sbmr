e945c2c640f733c926a3374638659238041062ed
Nick Strayer <nick.strayer@gmail.com>
2019-11-08 14:27:46 -0600

    Added new Weighted_Sampler class that allows us to easily sample from a weighted vector.

---
+++ Weighted_Sampler.cpp
@@ -0,0 +1,68 @@
+#include "Weighted_Sampler.h" 
+
+
+// =======================================================
+// Constructor 
+// =======================================================
+Weighted_Sampler::Weighted_Sampler()
+{
+   // Setup with the default random seed
+  initialize_seed(-1);
+}
+
+// When seed is provided, pass onto initialize seed
+Weighted_Sampler::Weighted_Sampler(int random_seed)
+{
+  initialize_seed(random_seed);
+}
+
+void Weighted_Sampler::initialize_seed(int random_seed) 
+{
+  // If the random seed is the null value of -1, use the default
+  // computer derived random seed. 
+  if(random_seed == -1) 
+  {
+    //Will be used to obtain a seed for the random number engine
+    std::random_device rd;  
+    
+    //Standard mersenne_twister_engine seeded with rd()
+    rand_int_gen int_gen(rd()); 
+  } 
+  else 
+  {
+    // Otherwise, use the desired seed
+    rand_int_gen int_gen(random_seed); 
+  }
+  
+  // Setup the random uniform generation function
+  rand_unif_gen unif_gen(0.0, 1.0);
+}
+
+double Weighted_Sampler::draw_unif() 
+{
+  return unif_gen(int_gen);
+}
+
+int Weighted_Sampler::sample(std::vector<double> const &weights) 
+{
+  // Make sure weights sum to 1
+  std::vector<double> weights_norm = normalize_vector(weights);
+
+  // Draw a random uniform value
+  double random_value = draw_unif();
+
+  double current_sum = 0;
+  int current_pos;
+
+  // Scan up through weights, stopping when the current sum passes the drawn value
+  for (current_pos = 0; current_pos < weights_norm.size(); ++current_pos) {
+    current_sum += weights_norm[current_pos];
+
+    // If we've gone over the random value then we're done
+    if (current_sum >= random_value) break;
+  }
+
+  return current_pos;
+}
+
+
---
+++ Weighted_Sampler.h
@@ -0,0 +1,37 @@
+//=================================
+// include guard
+//=================================
+#ifndef __WEIGHTED_SAMPLER_INCLUDED__
+#define __WEIGHTED_SAMPLER_INCLUDED__
+
+// #include <unordered_set>
+#include <random>
+#include <vector>
+#include "helpers.h"
+
+typedef std::mt19937 rand_int_gen;
+typedef std::uniform_real_distribution<> rand_unif_gen;
+
+//=================================
+// Main class declaration
+//=================================
+class Weighted_Sampler {
+  public:
+    // Constructor
+    Weighted_Sampler();
+    Weighted_Sampler(int random_seed);
+     
+    // ==========================================
+    // Attributes
+    rand_int_gen   int_gen;      // Generates random integers
+    rand_unif_gen  unif_gen;     // Generates random uniforms
+    
+    // ==========================================
+    // Methods   
+    void     initialize_seed(int random_seed);                // Sets stuff up for us, used to decide if we want to have deterministic seeding
+    double   draw_unif();                                     // Return random uniform value between 0 and 1.
+    int      sample(std::vector<double> const &weights);
+      
+  };
+
+#endif
