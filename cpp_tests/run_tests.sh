e42bd9dad04ff5ff4c1fd22b2041aa6514008593
Nick Strayer <nick.strayer@gmail.com>
2019-11-14 11:20:27 -0600

    Cleaned up entropy test a bit

--- cpp_tests/run_tests.sh
+++ cpp_tests/run_tests.sh
@@ -9,20 +9,20 @@ g++ -std=c++11 -c Node.cpp SBM.cpp helpers.cpp Sampler.cpp
 
 # g++ -std=c++11 -c Node.cpp helpers.cpp
 
+# 
+# echo "=============================================================================\nTesting Sampler.cpp"
+# echo "=============================================================================\n"
+# g++ -std=c++11 \
+#   -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
+#   -pthread cpp_tests/test_sampler.cpp Sampler.o helpers.o Node.o\
+#   /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
+# ./my_googletest
 
 echo "=============================================================================\nTesting Node.cpp"
 echo "=============================================================================\n"
 g++ -std=c++11 \
   -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
   -pthread cpp_tests/test_node.cpp Node.o helpers.o\
-  /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
-./my_googletest
-
-echo "=============================================================================\nTesting Sampler.cpp"
-echo "=============================================================================\n"
-g++ -std=c++11 \
-  -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
-  -pthread cpp_tests/test_sampler.cpp Sampler.o helpers.o Node.o\
   /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
 ./my_googletest
 
