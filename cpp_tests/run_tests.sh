f5e1e6f4e19a6b98122431abef2441b63cd246db
Nick Strayer <nick@nicks-mbp.attlocal.net>
2019-11-17 13:39:29 -0600

    Added instructions on building c++ testing framework and also made the test running script a bit more portable by parametarizing the test library location

--- cpp_tests/run_tests.sh
+++ cpp_tests/run_tests.sh
@@ -1,41 +1,40 @@
 echo $PWD
 
+# Set location of the google test library
+GTEST_LIB=/Users/nick/cpp_code/googletest/googletest/include/
+LIBGTEST=/Users/nick/cpp_code/googletest/install/lib/libgtest.a
 
 echo "=============================================================================\nCompiling files..."
 echo "=============================================================================\n"
-
 # Compile files so we can link
 g++ -std=c++11 -c Node.cpp SBM.cpp helpers.cpp Sampler.cpp
 
-# g++ -std=c++11 -c Node.cpp helpers.cpp
 
-# 
-# echo "=============================================================================\nTesting Sampler.cpp"
-# echo "=============================================================================\n"
-# g++ -std=c++11 \
-#   -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
-#   -pthread cpp_tests/test_sampler.cpp Sampler.o helpers.o Node.o\
-#   /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
-# ./my_googletest
+echo "=============================================================================\nTesting Sampler.cpp"
+echo "=============================================================================\n"
+g++ -std=c++11 \
+  -isystem ${GTEST_LIB} \
+  -pthread cpp_tests/test_sampler.cpp Sampler.o helpers.o Node.o\
+  ${LIBGTEST} -o my_googletest
+./my_googletest
+
 
 echo "=============================================================================\nTesting Node.cpp"
 echo "=============================================================================\n"
 g++ -std=c++11 \
-  -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
+  -isystem ${GTEST_LIB} \
   -pthread cpp_tests/test_node.cpp Node.o helpers.o\
-  /home/nick/cpp_dev/gtest_build/lib/libgtest.a -o my_googletest
+  ${LIBGTEST} -o my_googletest
 ./my_googletest
+
 
 echo "=============================================================================\nTesting SBM.cpp"
 echo "=============================================================================\n"
 g++ -std=c++11 \
-  -isystem /home/nick/cpp_dev/googletest/googletest/include/ \
+  -isystem ${GTEST_LIB} \
   -pthread cpp_tests/test_sbm.cpp Node.o SBM.o helpers.o Sampler.o\
-  /home/nick/cpp_dev/gtest_build/lib/libgtest.a \
-  -o my_googletest
+  ${LIBGTEST} -o my_googletest
 ./my_googletest
-
-
 
 
 # Cleanup the compiled files so RCpp doesn't get mad
