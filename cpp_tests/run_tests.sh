b82fa4d68dac6a8b8619e85ced0d5367b2188c32
Nick Strayer <nick@nicks-mbp.dhcp.mc.vanderbilt.edu>
2019-11-19 16:23:03 -0600

    Modified tests running to keep all activity within cpp_tests/ subdirectory

--- cpp_tests/run_tests.sh
+++ cpp_tests/run_tests.sh
@@ -5,13 +5,16 @@ echo "==========================================================================
 # # Compile files so we can link
 # g++ -std=c++11 -c Node.cpp SBM.cpp helpers.cpp Sampler.cpp
 
-# g++ -std=c++11 cpp_tests/tests-main.cpp -c 
+# g++ -std=c++11 cpp_tests/tests-main.cpp -c -o cpp_tests/tests-main.o
 
 g++ -std=c++11 -c Node.cpp SBM.cpp helpers.cpp Sampler.cpp
 
-g++ -std=c++11 tests-main.o Node.o helpers.o Sampler.o cpp_tests/tests-node.cpp cpp_tests/tests-sampler.cpp -o my_tests.o 
+g++ -std=c++11 cpp_tests/tests-main.o Node.o helpers.o Sampler.o cpp_tests/tests-node.cpp cpp_tests/tests-sampler.cpp -o cpp_tests/run_tests.o 
 
-./my_tests.o
+./cpp_tests/run_tests.o
+
+# Cleanup the compiled classes so RCpp doesn't get mad
+rm *.o
 
 # echo "=============================================================================\nTesting Sampler.cpp"
 # echo "=============================================================================\n"
