2c6f2d68b60ae3705062bb79518df05d8bfea51a
Nick Strayer <nick@nicks-mbp.dhcp.mc.vanderbilt.edu>
2019-12-03 15:20:36 -0600

    Changed debug to look at network building code

--- debugging/debug.cpp
+++ debugging/debug.cpp
@@ -12,49 +12,36 @@ int main(int argc, char **argv)
 {
   // Setup simple SBM model
  // Node level
-  NodePtr a1 = std::make_shared<Node>("a1", 0, 1);
-  NodePtr a2 = std::make_shared<Node>("a2", 0, 1);
-  NodePtr a3 = std::make_shared<Node>("a3", 0, 1);
-  NodePtr b1 = std::make_shared<Node>("b1", 0, 2);
-  NodePtr b2 = std::make_shared<Node>("b2", 0, 2);
-  NodePtr b3 = std::make_shared<Node>("b3", 0, 2);
-  
-  // First level / groups
-  NodePtr a11 = std::make_shared<Node>("a11", 1, 1);
-  NodePtr a12 = std::make_shared<Node>("a12", 1, 1);
-  NodePtr b11 = std::make_shared<Node>("b11", 1, 2);
-  NodePtr b12 = std::make_shared<Node>("b12", 1, 2);
-  
-  // Second level / super groups
-  NodePtr a21 = std::make_shared<Node>("a21", 2, 1);
-  NodePtr b21 = std::make_shared<Node>("b21", 2, 2);
-  
 
-  a1->set_parent(a11);
-  a2->set_parent(a12);
-  a3->set_parent(a12);
-  
-  b1->set_parent(b11);
-  b2->set_parent(b11);
-  b3->set_parent(b12);
-  
-  a11->set_parent(a21);
-  a12->set_parent(a21);
-  
-  b11->set_parent(b21);
-  b12->set_parent(b21);
-  
-  Node::connect_nodes(a1, b1);
-  Node::connect_nodes(a1, b2);
-  Node::connect_nodes(a2, b1);
-  Node::connect_nodes(a2, b2);
-  Node::connect_nodes(a3, b2);
-  Node::connect_nodes(a3, b3);
-  
-  
-  // Swap parents of a2 and b2 nodes
-  a2->set_parent(a11);
-  b2->set_parent(b12);
-      
+  SBM my_SBM;
+
+  NodePtr a1 = my_SBM.add_node("a1", 0, 0);
+  NodePtr a2 = my_SBM.add_node("a2", 0, 0);
+  NodePtr a3 = my_SBM.add_node("a3", 0, 0);
+  NodePtr b1 = my_SBM.add_node("b1", 1, 0);
+  NodePtr b2 = my_SBM.add_node("b2", 1, 0);
+  NodePtr b3 = my_SBM.add_node("b3", 1, 0);
+  NodePtr a11 = my_SBM.add_node("a11", 0, 1);
+  NodePtr a12 = my_SBM.add_node("a12", 0, 1);
+  NodePtr b11 = my_SBM.add_node("b11", 1, 1);
+  NodePtr b12 = my_SBM.add_node("b12", 1, 1);
+
+
+  my_SBM.add_connection(a1, b1);
+  my_SBM.add_connection(a1, b2);
+  my_SBM.add_connection(a1, b3);
+  my_SBM.add_connection(a2, b3);
+  my_SBM.add_connection(a3, b2);
+
+
+  my_SBM.set_node_parent(a2, a11);
+  my_SBM.set_node_parent(a1, a11);
+  my_SBM.set_node_parent(b1, b11);
+  my_SBM.set_node_parent(a3, a12);
+  my_SBM.set_node_parent(b3, b12);
+  my_SBM.set_node_parent(b2, b11);
+
+
+
   return 0;
 }
