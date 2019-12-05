ea7ab50ca0068ae846f032622b4636d37c85289a
Nick Strayer <nick.strayer@gmail.com>
2019-12-05 17:39:07 -0600

    Added an R-based show method to model so it returns the object's state when just called directly instead of an ugly pointer to object.

--- R/sbmR-package.R
+++ R/sbmR-package.R
@@ -9,3 +9,13 @@
 "_PACKAGE"
 
 Rcpp::loadModule("SBM", TRUE)
+
+# Set show method for object so we get something other than an ugly pointer to
+# object
+setClass("Rcpp_SBM")
+setMethod( "show", "Rcpp_SBM" , function(object) {
+  print(object$get_state())
+} )
+
+
+
