f2656089988df95740fdbfa6f6ca1f157340b720
Nick Strayer <nick.strayer@gmail.com>
2019-12-11 17:40:51 -0600

    Added docs to get_combination_indices and renamed the utils file to be general instead of just for the pipe operator.

---
+++ R/utils.R
@@ -0,0 +1,49 @@
+#' Pipe operator
+#'
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
+
+
+#' Get Combination Indices
+#'
+#' Generates a list with two arrays of indices that make up complete enumeration
+#' of all combinations (order does _not_ matter) of elements of vector of length
+#' `n`.
+#'
+#' @param n Number of elements in vector to get combinations of
+#' @param repeats Do we allow repeats in pairs? E.g. should a-a, b-b, etc. be allowed in generated pairs?
+#'
+#' @return A list with two vectors (`$a` and `$b`) containing integer indices of elements for every pair.
+#'
+#' @examples
+#'
+#' my_letters <- head(letters, 5)
+#' letter_pairs <- get_combination_indices(length(my_letters), repeats = FALSE)
+#'
+#' dplyr::tibble(
+#'   letter_1 = letters[letter_pairs$a],
+#'   letter_2 = letters[letter_pairs$b]
+#' )
+#'
+get_combination_indices <- function(n, repeats = FALSE){
+
+  rep_counts <- n:1
+
+  if(!repeats){
+    rep_counts <- rep_counts - 1
+  }
+
+  list(
+    a = rep(1:n, times = rep_counts),
+    b = purrr::flatten_int( purrr::map(rep_counts, ~{tail(1:n, .x)}) )
+  )
+}
+
