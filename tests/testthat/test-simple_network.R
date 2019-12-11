ab4e7439ccd3c63196a2f4b67a0a9b3184c41a67
Nick Strayer <nick.strayer@gmail.com>
2019-12-11 16:51:21 -0600

    Updated logic for figuring out number of expected edges because of switch to combination generation of edge pairs.

--- tests/testthat/test-simple_network.R
+++ tests/testthat/test-simple_network.R
@@ -1,24 +1,28 @@
 test_that("Proper number of connections happens", {
 
-  n_edges <- 5
+  n_nodes <- 5
   prob_of_connection <- 0.5
-  n_samples <- 50
+  n_samples <- 100
 
 
   sim_avg_con_count <- function(self_connections){
     1:n_samples %>%
       purrr::map_int(~{
-        nrow(sim_simple_network(n_edges, prob_of_connection, allow_self_connections = self_connections))
+        nrow(sim_simple_network(n_nodes, prob_of_connection, allow_self_connections = self_connections))
       }) %>%
       mean()
   }
 
-  expected_num_edges_wo_self_cons <- n_edges*prob_of_connection*(n_edges-1)
-  expected_num_edges_w_self_cons <- n_edges*prob_of_connection*n_edges
 
+  # Calculate the expected number of edges. This is divided by two because we
+  # only draw unique combos of groups so if we have edges for nodes a->b we wont
+  # also have b->a.
+  expected_num_edges_w_self_cons <- choose(n_nodes + 1, 2) * prob_of_connection
   edges_per_w_self_cons <- sim_avg_con_count(TRUE)
-  edges_per_wo_self_cons <- sim_avg_con_count(FALSE)
-
   expect_equal(edges_per_w_self_cons, expected_num_edges_w_self_cons, tolerance = 0.1)
+
+
+  expected_num_edges_wo_self_cons <- choose(n_nodes, 2) * prob_of_connection
+  edges_per_wo_self_cons <- sim_avg_con_count(FALSE)
   expect_equal(edges_per_wo_self_cons, expected_num_edges_wo_self_cons, tolerance = 0.1)
 })
