c74055b0f3e5e39fc74195210a2feacd789b0028
Nick Strayer <nick.strayer@gmail.com>
2019-12-17 13:28:50 -0600

    Added method wrapper in R for new collapse run method

---
+++ R/collapse_run.R
@@ -0,0 +1,45 @@
+#' Run Agglomerative Merging to targer a range of group numbers
+#'
+#' @inheritParams collapse_groups
+#' @param start_group_num Fewest number of groups to test
+#' @param end_group_num Highest number of groups to test
+#'
+#' @return Tibble with three columns with rows corresponding to the result of
+#'   each merge step:  `entropy`, `num_groups` left in model, and a list column
+#'   of `state` which is the state dump dataframe for model at end of merge.
+#' @export
+#'
+#' @examples
+collapse_run <- function(
+  sbm,
+  level = 0,
+  sigma = 2,
+  start_group_num = 1,
+  end_group_num = 10,
+  num_group_proposals = 5,
+  num_mcmc_sweeps = 10,
+  beta = 1.5
+){
+
+  # Set free parameters
+  sbm$BETA <- beta
+  sbm$SIGMA <- sigma
+  sbm$N_CHECKS_PER_GROUP <- num_group_proposals
+  sbm$GREEDY <- FALSE
+
+  collapse_results <- sbm$collapse_run(
+    as.integer(level),
+    as.integer(num_mcmc_sweeps),
+    as.integer(start_group_num),
+    as.integer(end_group_num)
+  )
+
+  purrr::map_dfr(
+    collapse_results,
+    ~dplyr::tibble(entropy = .$entropy,
+                   num_groups = .$num_groups)
+  ) %>%
+    dplyr::mutate(state = purrr::map(collapse_results, 'state'))
+}
+
+
