12c1da4ccf5e898a83af0afda4705b90ef5100c4
Nick Strayer <nick.strayer@gmail.com>
2019-12-18 12:10:47 -0600

    Updated the best collapse state choosing function to allow for arbitrary functions to score results and also added a built in option for scoring be deviance from the rolling mean

--- R/choose_best_collapse_state.R
+++ R/choose_best_collapse_state.R
@@ -1,25 +1,47 @@
 #' Choose and load best model state from agglomerative collapsing algorithm
 #'
 #' @inheritParams mcmc_sweep
-#' @param collapse_results Results of running agglomerative collapse algorithm on sbm with \code{\link{collapse_groups}}.
-#' @param heuristic How the best partitioning is defined. Currently only `'lowest' `is supported which chooses the point with lowest entropy.
+#' @param collapse_results Results of running agglomerative collapse algorithm
+#'   on sbm with \code{\link{collapse_groups}}.
+#' @param heuristic How the best partitioning is defined. Takes either a
+#'   function that takes an entropy vector or a string labeling algorithm.
+#'   Currently only `lowest` and `dev_from_rolling_mean` are supported.
+#' @verbose Should model tell you what step was chosen (`TRUE` or `FALSE`)?
 #'
-#' @return SBM model updated to match state from the merge step with 'best' partitioning.
+#' @return SBM model updated to match state from the merge step with 'best'
+#'   partitioning.
 #' @export
 #'
 #' @examples
 #'
-choose_best_collapse_state <- function(sbm, collapse_results, heuristic = 'lowest'){
+choose_best_collapse_state <- function(sbm, collapse_results, heuristic = 'lowest', verbose = FALSE){
 
-  if(heuristic == 'lowest'){
+  heuristic_is_function <- 'function' %in% class(heuristic)
 
-    best_state <- collapse_results %>%
-      filter(entropy == min(entropy)) %>%
-      pull('state') %>%
-      pluck(1)
+
+  if(heuristic_is_function){
+    score_func <- heuristic
   } else {
-    stop("Other choice heuristics not yet implemented.")
+    if(heuristic == 'lowest'){
+      score_func <-  function(vec) -vec
+    } else if(heuristic == 'dev_from_rolling_mean'){
+      score_func <- function(vec) rolling_mean(vec) - vec
+    } else {
+      stop("Other choice heuristics not yet implemented.")
+    }
   }
 
-  sbm %>% load_from_state(best_state)
+  # Apply the heuristic on the entropy column and choose the higheset value
+  best_state <- collapse_results %>%
+    mutate(score = score_func(entropy)) %>%
+    filter(score == max(score))
+
+  if(verbose){
+    n <- best_state$num_groups[1]
+    entropy <- best_state$entropy[1]
+    print(glue("Choosing collapse with {n} groups and an entropy of {entropy}."))
+  }
+
+  # Load best state into model and return
+  sbm %>% load_from_state(pluck(best_state$state, 1))
 }
