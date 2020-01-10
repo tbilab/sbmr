480d4abaf050887bdf00bd7f533cf6e78f8afc71
Nick Strayer <nick.strayer@gmail.com>
2020-01-09 21:15:30 -0700

    Added the pair tracking argument in the examples.

--- R/mcmc_sweep.R
+++ R/mcmc_sweep.R
@@ -43,6 +43,9 @@
 #' # Look at the per-sweep level information
 #' sweep_results$sweep_info
 #'
+#' # Get idea of node-pair similarity by looking at how often every pair of nodes is connected over sweeps
+#' my_sbm %>% mcmc_sweep(num_sweeps = 4, track_pairs = TRUE)
+#'
 mcmc_sweep <- function(sbm, num_sweeps = 1, variable_num_blocks = TRUE, track_pairs = FALSE, level = 0){
 
   results <- sbm$mcmc_sweep(as.integer(level),
