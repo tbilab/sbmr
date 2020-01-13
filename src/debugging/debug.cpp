3a67aafe2c38e06b4a33e592e34c299fa40a09a5
Nick Strayer <nick.strayer@gmail.com>
2020-01-13 11:23:54 -0600

    Updated debugging script to look at entropy delta stuff and also the runner for debugging to deal with new helper.cpp-less formating.

--- src/debugging/debug.cpp
+++ src/debugging/debug.cpp
@@ -2,46 +2,57 @@
 // tests that are failing and then stepping through to see what's going on.
 
 #include <iostream>
-#include "../helpers.h"
 #include "../SBM.h"
 #include "../cpp_tests/network_builders.cpp"
 
-
-
 int main(int argc, char **argv)
 {
+
+  int num_sweeps = 10;
+
   // Setup simple SBM model
- // Node level
+  SBM my_SBM = build_simple_SBM();
 
-  SBM my_SBM;
+  auto all_nodes = my_SBM.get_level(0);
 
-  NodePtr a1 = my_SBM.add_node("a1", 0, 0);
-  NodePtr a2 = my_SBM.add_node("a2", 0, 0);
-  NodePtr a3 = my_SBM.add_node("a3", 0, 0);
-  NodePtr b1 = my_SBM.add_node("b1", 1, 0);
-  NodePtr b2 = my_SBM.add_node("b2", 1, 0);
-  NodePtr b3 = my_SBM.add_node("b3", 1, 0);
-  NodePtr a11 = my_SBM.add_node("a11", 0, 1);
-  NodePtr a12 = my_SBM.add_node("a12", 0, 1);
-  NodePtr b11 = my_SBM.add_node("b11", 1, 1);
-  NodePtr b12 = my_SBM.add_node("b12", 1, 1);
+  for (int i = 0; i < num_sweeps; i++)
+  {
+    std::cout << "mcmc sweep: " << i << std::endl;
+    // Loop through all nodes
+    for (auto node_to_move_it = all_nodes->begin();
+         node_to_move_it != all_nodes->end();
+         node_to_move_it++)
+    {
+      NodePtr node_to_move = node_to_move_it->second;
 
+      // Calculate current model entropy
+      double pre_entropy = my_SBM.compute_entropy(0);
 
-  my_SBM.add_edge(a1, b1);
-  my_SBM.add_edge(a1, b2);
-  my_SBM.add_edge(a1, b3);
-  my_SBM.add_edge(a2, b3);
-  my_SBM.add_edge(a3, b2);
+      // Choose random group for node to join
+      NodePtr group_to_move_to = my_SBM.sampler.sample(my_SBM.get_nodes_of_type_at_level(node_to_move->type, 1));
 
 
-  a2->set_parent(a11);
-  a1->set_parent(a11);
-  b1->set_parent(b11);
-  a3->set_parent(a12);
-  b3->set_parent(b12);
-  b2->set_parent(b11);
+      std::cout << "Moving node " << node_to_move->id << " from " <<  
+                   (node_to_move->parent)->id << " to " <<
+                    group_to_move_to->id << std::endl;
 
+      // Get move proposal report for move
+      Proposal_Res proposal_vals = my_SBM.make_proposal_decision(node_to_move, group_to_move_to);
 
+      double reported_entropy_delta = proposal_vals.entropy_delta;
+
+      // Move node
+      node_to_move->set_parent(group_to_move_to);
+
+      // Take new model entropy
+      double post_entropy = my_SBM.compute_entropy(0);
+
+      std::cout << "(True : Reported) -> ("
+                << std::to_string(post_entropy - pre_entropy) << " , " << reported_entropy_delta << ")"
+                << std::endl;
+
+    } // End node loop
+  }   // End iteration loop
 
   return 0;
 }
