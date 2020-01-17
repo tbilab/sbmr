6e7bbbcaa0a7e8c2a101339ff0463bb76cfbbe7e
Nick Strayer <nick.strayer@gmail.com>
2020-01-17 16:15:02 -0600

    Added two scratch scripts used to verify the behavior of both the MCMC sweeping and the agglomerative merging functions.

---
+++ tests/manual/agglomerative_collapsing_scratch.R
@@ -0,0 +1,120 @@
+library(tidyverse)
+library(glue)
+library(sbmR)
+
+set.seed(42)
+
+n_blocks <- 3    # Total number of blocks
+block_size <- 15 # How many nodes in each block
+
+network <- sim_basic_block_network(
+  n_blocks = n_blocks,
+  n_nodes_per_block = block_size,
+  return_edge_propensities = TRUE
+)
+
+my_sbm <- create_sbm(network)
+
+collapse_results <- my_sbm %>%
+  collapse_run(num_block_proposals = 15,
+               sigma = 6,
+               num_mcmc_sweeps = 10,
+               num_final_blocks = 1:15,
+               parallel = TRUE)
+
+collapse_results %>%
+  visualize_collapse_results(heuristic = 'nls_residual') +
+  geom_vline(xintercept = n_blocks, color = 'orangered') +
+  geom_vline(xintercept = 2*n_blocks, color = 'orangered')
+
+my_sbm <- choose_best_collapse_state(my_sbm, collapse_results, heuristic =function(e,b){as.numeric(b == n_blocks)}, verbose = TRUE)
+
+nodes_w_assignments <- my_sbm %>%
+  get_state() %>%
+  filter(level == 0) %>%
+  right_join(network$nodes, by = 'id')%>%
+  rename(inferred = parent)
+
+nodes_w_assignments %>%
+  group_by(inferred) %>%
+  summarise(n_unique_true = length(unique(block))) %>%
+  arrange(-n_unique_true)
+
+nodes_w_assignments %>%
+  group_by(block) %>%
+  summarise(n_unique_assigned = length(unique(inferred))) %>%
+  arrange(-n_unique_assigned)
+
+
+my_sbm %>%
+  initialize_blocks(n_blocks)
+
+num_sweeps <- 2
+
+sweep_results <- mcmc_sweep(my_sbm, num_sweeps = num_sweeps, track_pairs = FALSE, variable_num_blocks = TRUE)
+
+sweep_results$sweep_info %>%
+  mutate(sweep = 1:n(),
+         label = 'eps = 0.1'
+         # entropy_delta = cumsum(entropy_delta)
+         ) %>%
+  # filter(sweep > 25) %>%
+  pivot_longer(entropy_delta:num_nodes_moved) %>%
+  ggplot(aes(x = sweep, y = value)) +
+  geom_line() +
+  facet_grid(name~., scales = "free_y") +
+  labs(
+    title = glue::glue('Result of {num_sweeps} MCMC sweeps'),
+    subtitle = "Entropy Delta of sweep and number of nodes moved for sweep"
+  )
+#
+# my_sbm %>%
+#   get_state() %>%
+#   filter(level == 0) %>%
+#   right_join(network$nodes, by = 'id')%>%
+#   rename(inferred = parent) %>%
+#   group_by(inferred) %>%
+#   summarise(n_unique_true = length(unique(block))) %>%
+#   arrange(-n_unique_true)
+#
+#
+#
+# e_delta = -4.926308; P_pre = 0.599292; P_post = 0.822462
+# e_delta = -75.862098; P_pre = 35.979662; P_post = -1606.925421
+#
+# exp(-e_delta)*(P_post/P_pre)
+#
+# e_delta <- -71.744486
+# P_pre <- 119.900000
+# P_post <-  46.900000
+#
+#
+# sweep_results$sweep_info %>% arrange(entropy_delta)
+# # collapse_results <- my_sbm %>%
+# #   collapse_run(num_block_proposals = 15,
+# #                sigma = 6,
+# #                num_mcmc_sweeps = 15,
+# #                num_final_blocks = 1:15,
+# #                parallel = TRUE)
+# #
+# # visualize_collapse_results(collapse_results, heuristic = 'dev_from_rolling_mean') +
+# #   geom_vline(xintercept = n_blocks, color = 'orangered')
+#
+# tibble(
+#   x = seq(-2, 2, length.out = 30),
+#   y = exp(x)
+# ) %>%
+#   ggplot(aes( x = x, y = y) ) +
+#   geom_line() +
+#   geom_hline(yintercept = 1)
+#
+#
+# my_sbm %>% compute_entropy()
+#
+# plot_sweep_results(sweep_df)
+
+tibble(
+  x = seq(0, 1, length.out = 130),
+  y = log(x)
+) %>%
+  ggplot(aes(x = x, y = y)) + geom_line()
---
+++ tests/manual/mcmc_sweep_scratch.R
@@ -0,0 +1,179 @@
+library(tidyverse)
+library(glue)
+library(sbmR)
+
+set.seed(42)
+
+# n_blocks <- 4    # Total number of blocks
+# block_size <- 50 # How many nodes in each block
+n_blocks <- 3   # Total number of blocks
+block_size <- 30 # How many nodes in each block
+
+network <- sim_basic_block_network(
+  n_blocks = n_blocks,
+  n_nodes_per_block = block_size,
+  return_edge_propensities = TRUE
+)
+
+
+# network %>%
+#   pluck("nodes") %>%
+#   glue_data("my_SBM.add_node(\"{id}\", 0);")
+#
+# network %>%
+#   pluck("edges") %>%
+#   glue_data("my_SBM.add_edge(\"{from}\", \"{to}\");")
+
+# visualize_network(network)
+
+
+my_sbm <- create_sbm(network) %>%
+  initialize_blocks(n_blocks)
+
+start_entropy <- my_sbm %>% compute_entropy()
+num_sweeps <- 25
+sweep_results <- mcmc_sweep(my_sbm, num_sweeps = num_sweeps, track_pairs = FALSE, variable_num_blocks = FALSE, verbose = FALSE)
+end_entropy <- my_sbm %>% compute_entropy()
+#
+# sweep_info <- read_csv(here::here("tests/manual/sweep_results.txt"))
+# sweep_info %>%
+#   filter(prob_of_accept < 40) %>%
+#   ggplot(aes(x = entropy_delta, y = prob_of_accept, color = move_accepted)) +
+#   geom_point()
+#
+# sweep_info %>%
+#   filter(prob_of_accept < 4) %>%
+#   ggplot(aes(x = prob_of_accept)) +
+#   geom_histogram(bins = 40)
+
+
+sweep_results$sweep_info %>%
+  mutate(sweep = 1:n()) %>%
+  pivot_longer(c(entropy_delta, num_nodes_moved)) %>%
+  ggplot(aes(x = sweep, y = value)) +
+    geom_line() +
+    geom_point() +
+    facet_grid(name~., scales = "free_y")
+
+node_states <- get_state(my_sbm) %>%
+  filter(level == 0)
+
+node_states %>%
+  count(parent)
+
+nodes_w_assignments <- node_states %>%
+  right_join(network$nodes, by = 'id')%>%
+  rename(inferred = parent)
+
+nodes_w_assignments %>%
+  group_by(inferred) %>%
+  summarise(n_unique_true = length(unique(block))) %>%
+  arrange(-n_unique_true)
+
+nodes_w_assignments %>%
+  group_by(block) %>%
+  summarise(n_unique_assigned = length(unique(inferred))) %>%
+  arrange(-n_unique_assigned)
+
+
+start_entropy <- compute_entropy(my_sbm)
+predicted_entropy <- start_entropy
+
+num_sweeps <- 20
+true_res <- c(start_entropy)
+reported_deltas <- c(0)
+predicted_res <- c(start_entropy)
+
+for(sweep in 1:num_sweeps){
+  # One sweep
+  sweep_results <- mcmc_sweep(my_sbm, num_sweeps = 1, track_pairs = FALSE, variable_num_blocks = FALSE)
+
+  # Extract entropy delta
+  delta <- sweep_results$sweep_info$entropy_delta[1]
+  reported_deltas <- c(reported_deltas, delta)
+  predicted_entropy <- predicted_entropy + (delta)
+
+  # Update values
+  true_res <- c(true_res, compute_entropy(my_sbm))
+  predicted_res <- c(predicted_res, predicted_entropy)
+}
+
+comparison_res <- tibble(
+  true = true_res,
+  delta = reported_deltas,
+  predicted = predicted_res,
+  sweep = 0:num_sweeps
+) %>%
+  mutate(difference = true-predicted)
+
+
+comparison_res %>%
+  mutate(true_delta = true - lag(true),
+         true_delta = ifelse(is.na(true_delta), 0, true_delta),
+         delta_new = delta) %>%
+  pivot_longer(c(delta_new, true_delta)) %>%
+  ggplot(aes(x = sweep, y = value, color = name)) +
+  geom_point() +
+  geom_line()
+
+
+
+comparison_res %>%
+  pivot_longer(c(true, predicted)) %>%
+  ggplot(aes(x = sweep, y = value, color = name)) +
+  geom_line()
+
+
+sweep_results <- mcmc_sweep(my_sbm, num_sweeps = num_sweeps, track_pairs = FALSE, variable_num_blocks = TRUE)
+
+sweep_stats <- sweep_results$sweep_info %>%
+  mutate(sweep = 1:n(),
+         label = 'eps = 0.1',
+         entropy = start_entropy + cumsum(-entropy_delta/2)
+  )
+# e_delta = -0.061816; P_move = 0.352967; P_rev = 0.421051; P_accept = 0.891756
+# exp(-e_delta)*(P_move/P_rev)
+
+sweep_stats  %>%
+  pivot_longer(c(entropy_delta, num_nodes_moved)) %>%
+  ggplot(aes(x = sweep, y = value)) +
+  geom_line() +
+  facet_grid(name~., scales = "free_y") +
+  labs(
+    title = glue::glue('Result of {num_sweeps} MCMC sweeps'),
+    subtitle = "Entropy Delta of sweep and number of nodes moved for sweep"
+  )
+
+true_final_entropy <- my_sbm %>% compute_entropy()
+
+sweep_stats %>%
+  ggplot(aes(x = sweep, y = entropy)) +
+  geom_line() +
+  geom_hline(yintercept = true_final_entropy)
+
+
+node_states <- get_state(my_sbm) %>%
+  filter(level == 0)
+
+node_states %>%
+  count(parent)
+
+nodes_w_assignments <- node_states %>%
+  right_join(network$nodes, by = 'id')%>%
+  rename(inferred = parent)
+
+nodes_w_assignments %>%
+  group_by(inferred) %>%
+  summarise(n_unique_true = length(unique(block))) %>%
+  arrange(-n_unique_true)
+
+nodes_w_assignments %>%
+  group_by(block) %>%
+  summarise(n_unique_assigned = length(unique(inferred))) %>%
+  arrange(-n_unique_assigned)
+
+visualize_network(edges = network$edges,
+                  nodes = nodes_w_assignments,
+                  node_color_col = 'block',
+                  node_shape_col = 'inferred',
+                  width = '100%')
