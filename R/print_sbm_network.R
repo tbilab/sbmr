829b7cb9572d1ffd01254cf975f8e9690204afc9
Nick Strayer <nick.strayer@gmail.com>
2020-02-05 15:12:37 -0600

    Added docs to print method

--- R/print_sbm_network.R
+++ R/print_sbm_network.R
@@ -1,6 +1,30 @@
+#' Print network
+#'
+#' @inheritParams initialize_model
+#'
+#' @return Output of the number of nodes and edges in model along with
+#'   indication of if a model has been initialized
+#' @export
+#'
+#' @examples
+#' # Build small object from simple edge dataframe
+#' small_edges <- dplyr::tribble(
+#'   ~a_node, ~b_node,
+#'   "a1"   , "b1"   ,
+#'   "a1"   , "b2"   ,
+#'   "a1"   , "b3"   ,
+#'   "a2"   , "b1"   ,
+#'   "a2"   , "b4"   ,
+#'   "a3"   , "b1"
+#' )
+#'
+#' sbm_net <- new_sbm_network(small_edges, edges_from_col = a_node, edges_to_col = b_node)
+#' print(sbm_net)
+#'
 print.sbm_network <- function(x){
-  N <- attr(sbm_net, "n_nodes")
-  E <- attr(sbm_net, "n_edges")
+
+  N <- attr(x, "n_nodes")
+  E <- attr(x, "n_edges")
   cat(glue::glue("SBM Network with {N} nodes and {E} edges."))
 
   if (!is.null(x$model)){
