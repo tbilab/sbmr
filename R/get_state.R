5e2efbb8b1709472729a55b17dd2340e8a08c17c
Nick Strayer <nick.strayer@gmail.com>
2020-02-06 11:48:31 -0600

    get state converted to s3 method and just uses the hopefully cached state attribute.

--- R/get_state.R
+++ R/get_state.R
@@ -1,16 +1,15 @@
-#' Dump state of model to dataframe
+#' Dump state of current SBM model to dataframe
 #'
 #' Exports the current state of the network as dataframe with each node as a row
-#' and columns for node id, parent id, node type, and node level.
+#' and columns for node id, parent id, node type, and node level. Can also be
+#' accomplished by `attr(sbm, 'state')`.
 #'
 #' @inheritParams add_node
 #'
 #' @return A dataframe describing the current state of the network: \describe{
-#'   \item{id}{id of node}
-#'   \item{parent}{id of node's parent. `none` if node has no parent.}
-#'   \item{type}{type of node}
-#'   \item{level}{level of node in network hierarchy}
-#' }
+#'   \item{id}{id of node} \item{parent}{id of node's parent. `none` if node has
+#'   no parent.} \item{type}{type of node} \item{level}{level of node in network
+#'   hierarchy} }
 #'
 #' @export
 #'
@@ -19,14 +18,20 @@
 #' # Build a bipartite network with 1 node and one block for each type
 #' my_sbm <- create_sbm() %>%
 #'   add_node('a1', type = 'a') %>%
-#'   add_node('a11', type = 'a', level = 1) %>%
-#'   add_node('b1', type = 'b') %>%
-#'   add_node('b11', type = 'b', level = 1) %>%
-#'   set_node_parent('a1', 'a11') %>%
-#'   set_node_parent('b1', 'b11')
+#'   add_node('b1', type = 'b')
 #'
 #' my_sbm %>% get_state()
 #'
-get_state <- function(sbm){
-  sbm$get_state()
+get_state <- function(x){
+  set_generic("get_state")
 }
+
+get_state.default <- function(x){
+  cat("get_state generic")
+}
+
+#' @export
+get_state.sbm_network <- function(x){
+  attr(verify_model(x), 'state')
+}
+
