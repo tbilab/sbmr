d95f867c311592f1881d6ecdc52b6773c89e956e
Nick Strayer <nick.strayer@gmail.com>
2020-02-06 16:51:48 -0600

    Reconfigured tests for merging to work with the new syntax.

--- tests/testthat/test-agglomerative_merging.R
+++ tests/testthat/test-agglomerative_merging.R
@@ -1,16 +1,11 @@
-
 test_that("Agglomerative merging with MCMC works", {
 
   n_blocks <- 2
   n_nodes_per_block <- 30
 
-  simulated_network <- sim_basic_block_network(n_blocks = n_blocks,
-                                               n_nodes_per_block = n_nodes_per_block,
-                                               return_edge_propensities = TRUE)
+  net <- sim_basic_block_network(n_blocks = n_blocks, n_nodes_per_block = n_nodes_per_block)
 
-  # Start with a random network
-  my_sbm <- create_sbm(simulated_network)
-  collapse_results <- collapse_blocks(my_sbm, num_mcmc_sweeps = 1, report_all_steps = TRUE)
+  collapse_results <- collapse_blocks(net, num_mcmc_sweeps = 1, report_all_steps = TRUE)
 
   blocks_per_collapse <- collapse_results$num_blocks
 
@@ -23,16 +18,10 @@ test_that("Agglomerative merging with MCMC works", {
 
 test_that("Agglomerative merging without MCMC works", {
 
-  n_blocks <- 2
-  n_nodes_per_block <- 30
-
-  simulated_network <- sim_basic_block_network(n_blocks = n_blocks,
-                                               n_nodes_per_block = n_nodes_per_block,
-                                               return_edge_propensities = TRUE)
-
   # Start with a random network
-  my_sbm <- create_sbm(simulated_network)
-  collapse_results <- collapse_blocks(my_sbm, num_mcmc_sweeps = 0, report_all_steps = TRUE)
+  net <- sim_basic_block_network(n_blocks = 2, n_nodes_per_block = 30)
+
+  collapse_results <- collapse_blocks(net, num_mcmc_sweeps = 0, report_all_steps = TRUE)
 
   blocks_per_collapse <- collapse_results$num_blocks
 
@@ -46,12 +35,8 @@ test_that("Agglomerative merging without MCMC works", {
 
 test_that("Requesting just the final merge step returns just the final merge step", {
 
-  # Start with a random network
-  my_sbm <- create_sbm(
-    sim_basic_block_network(n_blocks = 4, n_nodes_per_block = 30)
-  )
-
-  collapse_results <- my_sbm %>%
+  collapse_results <- sim_basic_block_network(n_blocks = 2,
+                                              n_nodes_per_block = 30) %>%
     collapse_blocks(desired_num_blocks = 4,
                     num_mcmc_sweeps = 0,
                     report_all_steps = FALSE)
@@ -59,21 +44,12 @@ test_that("Requesting just the final merge step returns just the final merge ste
   expect_equal(nrow(collapse_results), 1)
 })
 
-
 test_that("Collapse run works in sequential mode", {
 
-  # Start with a random network
-  my_sbm <- create_sbm(
-    sim_basic_block_network(n_blocks = 3, n_nodes_per_block = 30)
-  )
+  collapse_results <- sim_basic_block_network(n_blocks = 2, n_nodes_per_block = 30) %>%
+    collapse_run(num_final_blocks = 1:5, num_mcmc_sweeps = 3)
 
-  num_expected_results <- 5
-
-  collapse_results <- my_sbm %>%
-    collapse_run(num_final_blocks = 1:5,
-                 num_mcmc_sweeps = 3)
-
-  expect_equal(nrow(collapse_results), num_expected_results)
+  expect_equal(nrow(collapse_results), 5)
 })
 
 
