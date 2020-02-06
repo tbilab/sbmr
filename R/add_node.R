4f6f2807440ff696975d4c476cb4583a64e1fc60
Nick Strayer <nick.strayer@gmail.com>
2020-02-06 14:51:43 -0600

    Fixed bug where adding an existing node was still attempted and also fixed issue where sbm_network n_nodes attribute was not getting incremented properly.

--- R/add_node.R
+++ R/add_node.R
@@ -3,9 +3,12 @@
 #' Add a node to the network. Takes the node id (string), the node type
 #' (string), and the node level (int).
 #'
-#' @param sbm `sbm_network` object as created by \code{\link{new_sbm_network()}}.
+#' @param sbm `sbm_network` object as created by
+#'   \code{\link{new_sbm_network()}}.
 #' @param id Unique identifying name for node.
-#' @param type Type of node (string). This is used to distinguish multipartite networks. E.g. "person", or "publication", etc..
+#' @param type Type of node (string). This is used to distinguish multipartite
+#'   networks. E.g. "person", or "publication", etc..
+#' @inheritParams verify_model
 #'
 #' @return `sbm_network` object  with node added.
 #' @export
@@ -18,42 +21,51 @@
 #'
 #' get_state(my_sbm)
 #'
-add_node <- function(sbm, id, type = NULL){
+add_node <- function(sbm, id, type = NULL, show_messages = TRUE){
   UseMethod("add_node")
 }
 
-add_node.default <- function(sbm, id, type = NULL){
+add_node.default <- function(sbm, id, type = NULL, show_messages = TRUE){
   cat("add_node generic")
 }
 
 #' @export
-add_node.sbm_network <- function(sbm, id, type = NULL){
+add_node.sbm_network <- function(sbm, id, type = NULL, show_messages = TRUE){
 
-  node_not_in_network <- not_in(id, sbm_nodes$id)
+  node_not_in_network <- not_in(id, sbm$nodes$id)
   if(node_not_in_network){
 
     type_missing <- is.null(type)
 
     if(type_missing){
       type <- sbm$nodes$type[1]
-      warning(glue::glue("{id} node not in network but has no specified type. Defaulting to {from_node_type}"))
+      if(show_messages){
+        message(glue::glue("{id} node not in network but has no specified type. Defaulting to {type}"))
+      }
     }
 
     # Add node to nodes list
     sbm$nodes <- dplyr::bind_rows(sbm$nodes,
                                   dplyr::tibble(id = id, type = type))
+
+
+    # Before we start modifying the model object, first verify it
+    sbm <- verify_model(sbm)
+
+    # Add node to s4 model class
+    sbm$model$add_node(id, type, 0L)
+
+    # Update state with new nodes [inefficient]
+    attr(sbm, "state") <- sbm$model$get_state()
+
+    # Update number of nodes
+    attr(sbm, "n_nodes") <- attr(sbm, "n_nodes") + 1
+
   } else {
-    message(glue::glue("{id} node was already in network. No action taken."))
+    if(show_messages){
+      message(glue::glue("{id} node was already in network. No action taken."))
+    }
   }
-
-  # Before we start modifying the model object, first verify it
-  sbm <- verify_model(sbm)
-
-  # Add node to s4 model class
-  sbm$model$add_node(id, type, 0L)
-
-  # Update state with new nodes [inefficient]
-  attr(sbm, "state") <- sbm$model$get_state()
 
   sbm
 }
