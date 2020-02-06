38589a8b794b757330a2ea875565907f9ffbd877
Nick Strayer <nick.strayer@gmail.com>
2020-02-06 17:01:22 -0600

    Updated node edge count gathering tests for new syntax

--- tests/testthat/test-get_node_edge_counts_at_level.R
+++ tests/testthat/test-get_node_edge_counts_at_level.R
@@ -1,22 +1,19 @@
 test_that("Correct edge counts returned", {
-  simulated_network <- sim_basic_block_network(n_blocks = 5)
-  sbm <- simulated_network %>%
-    create_sbm() %>%
+
+  net <- sim_basic_block_network(n_blocks = 5) %>%
     initialize_blocks(5)
 
-  total_num_edges <- nrow(simulated_network$edges)
+  total_num_edges <- attr(net, 'n_edges')
 
-  # Get ids of all the nodes generated
-  sbm_state <- get_state(sbm)
-  node_ids <- sbm_state$id[sbm_state$level == 0]
-
-  # And get ids of all the blocks generated
-  block_ids <- sbm_state$id[sbm_state$level == 1]
+  # Get ids of all the nodes and blocks generated
+  sbm_state <- get_state(net)
+  node_ids <- sbm_state %>% dplyr::filter(level == 0) %>% dplyr::pull(id)
+  block_ids <- sbm_state %>% dplyr::filter(level == 1) %>% dplyr::pull(id)
 
   # Loop over all nodes and make sure they are not connected to any nodes they
   # shouldnt be and that they have reasonable edge counts
   node_ids %>% purrr::walk(function(node_id){
-    nodes_connections <- sbm %>% get_node_to_block_edge_counts(node_id)
+    nodes_connections <- net %>% get_node_to_block_edge_counts(node_id)
 
     expect_true(
       all(nodes_connections$id %in% block_ids)
