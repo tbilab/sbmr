58ca7df2afabc5ff7c4b5c7d93af1d0d390aef20
Nick Strayer <nick.strayer@gmail.com>
2020-02-07 11:04:40 -0600

    Made print method a bit more valuable

--- R/print_sbm_network.R
+++ R/print_sbm_network.R
@@ -9,27 +9,38 @@
 #' @export
 #'
 #' @examples
+#'
 #' # Build small object from simple edge dataframe
 #' small_edges <- dplyr::tribble(
 #'   ~a_node, ~b_node,
-#'   "a1"   , "b1"   ,
-#'   "a1"   , "b2"   ,
-#'   "a1"   , "b3"   ,
-#'   "a2"   , "b1"   ,
-#'   "a2"   , "b4"   ,
-#'   "a3"   , "b1"
+#'      "a1",    "b1",
+#'      "a1",    "b2",
+#'      "a1",    "b3",
+#'      "a2",    "b1",
+#'      "a2",    "b4",
+#'      "a3",    "b1"
 #' )
 #'
 #' sbm_net <- new_sbm_network(small_edges, edges_from_col = a_node, edges_to_col = b_node)
 #' print(sbm_net)
 #'
-print.sbm_network <- function(x){
+print.sbm_network <- function(x, rows_for_preview = 5){
 
   N <- attr(x, "n_nodes")
   E <- attr(x, "n_edges")
-  cat(glue::glue("SBM Network with {N} nodes and {E} edges."), "\n")
+  cat(glue::glue("SBM Network with {N} nodes and {E} edges."), "\n", "\n")
+
+  cat("Nodes: ")
+  print(head(x$nodes, rows_for_preview))
+  cat("...", "\n","\n")
+
+  cat("Edges: ")
+  print(head(x$edges, rows_for_preview))
+  cat("...", "\n","\n")
 
   if (!is.null(x$model)){
-    cat("The network has an initialized model object.", "\n")
+    cat(glue::glue("Model", "\n",
+                   "Currently has {get_num_blocks(x)} blocks",
+                   "\n"))
   }
 }
