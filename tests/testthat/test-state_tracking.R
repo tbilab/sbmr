460c93ae7aaf581ed2717669642b220a136f7afd
Nick Strayer <nick.strayer@gmail.com>
2020-02-07 10:44:31 -0600

    Added simple test for the new update_state method

--- tests/testthat/test-state_tracking.R
+++ tests/testthat/test-state_tracking.R
@@ -43,6 +43,58 @@ test_that("State tracking returns the correct state", {
 })
 
 
+test_that("State updating method", {
+
+  edges <- dplyr::tribble(
+    ~from,   ~to,
+     "a1",  "b1",
+     "a1",  "b2",
+     "a1",  "b3",
+     "a2",  "b1",
+     "a2",  "b4",
+     "a3",  "b1"
+  )
+
+  nodes <- dplyr::tribble(
+    ~id,   ~type,
+    "a1", "node",
+    "a2", "node",
+    "a3", "node",
+    "b1", "node",
+    "b2", "node",
+    "b3", "node",
+    "b4", "node"
+  )
+
+  new_state <- dplyr::tribble(
+     ~id,          ~parent,   ~type,  ~level,
+    "a1",       "a_parent",  "node",      0L,
+    "a2",       "a_parent",  "node",      0L,
+    "a3",       "a_parent",  "node",      0L,
+    "b1",       "b_parent",  "node",      0L,
+    "b2",       "b_parent",  "node",      0L,
+    "b3",       "b_parent",  "node",      0L,
+    "b4",       "b_parent",  "node",      0L,
+    "a_parent", "none",      "node",      1L,
+    "b_parent", "none",      "node",      1L,
+  )
+
+  net <- new_sbm_network(edges = edges, nodes = nodes)
+
+  # before updating state the state should not equal the desired new state
+  expect_false( isTRUE(dplyr::all_equal(new_state, attr(net, 'state'))))
+  expect_false( isTRUE(dplyr::all_equal(new_state, net$model$get_state())))
+
+  # Update state using update_state() method
+
+  net <- net %>% update_state(new_state)
+
+  # Now the states should be identical
+  expect_true( dplyr::all_equal(new_state, attr(net, 'state')))
+  expect_true( dplyr::all_equal(new_state, net$model$get_state()))
+
+})
+
 test_that("No costly duplication of S4 class is done when assigning s3 class copies.", {
 
   net <- sim_basic_block_network(n_blocks = 3,
