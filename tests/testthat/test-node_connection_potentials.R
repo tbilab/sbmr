dc0627a6d8171d1f15a43e981fe8997a6106ff0a
Nick Strayer <nick.strayer@gmail.com>
2020-02-17 15:35:24 -0600

    Added a (currently failing) test for new node connection specification argument

---
+++ tests/testthat/test-node_connection_potentials.R
@@ -0,0 +1,62 @@
+# Or if adding a node dataframe as well, arbitrary polypartite structure is possible
+edges_tripartite <- dplyr::tribble(
+  ~from,    ~to,
+     "a1", "b1",
+     "a1", "b2",
+     "a2", "b1",
+     "b1", "c1",
+     "b2", "c1",
+     "b2", "c2"
+)
+
+nodes_tripartite <- dplyr::tribble(
+  ~id, ~type,
+  "a1", "a",
+  "a2", "a",
+  "b1", "b",
+  "b2", "b",
+  "c1", "c",
+  "c2", "c"
+)
+
+new_sbm_network(edges = edges_tripartite,
+                nodes = nodes_tripartite)
+
+
+
+test_that("Error thrown when stated connection potentials are violated", {
+  edges_tripartite <- dplyr::tribble(
+    ~from,  ~to,
+     "a1", "b1",
+     "a1", "b2",
+     "a2", "b1",
+     "a1", "c1",
+     "a2", "c1",
+     "a2", "c2",
+     "b1", "c1" # the bad connection
+  )
+
+  nodes_tripartite <- dplyr::tribble(
+    ~id, ~type,
+    "a1", "a",
+    "a2", "a",
+    "b1", "b",
+    "b2", "b",
+    "c1", "c",
+    "c2", "c"
+  )
+
+  # a nodes can connect to b and c but b and c can not connect
+  allowed_edge_pairs <- dplyr::tribble(
+    ~from,  ~to,
+      "a", "b",
+      "a", "c"
+  )
+
+  expect_error(
+    new_sbm_network(edges = edges_tripartite,
+                    nodes = nodes_tripartite,
+                    allowed_edge_pairs = allowed_edge_pairs)
+  )
+
+})
