4de0c3012218f89ae9c9a4cc32ff7b3c7ebde9cd
Nick Strayer <nick.strayer@gmail.com>
2020-02-18 16:27:56 -0600

    Add node now works handles node types properly.

--- R/add_node.R
+++ R/add_node.R
@@ -46,29 +46,24 @@ add_node.sbm_network <- function(sbm, id, type = NULL, show_messages = TRUE){
 
   type_missing <- is.null(type)
   node_not_in_network <- not_in(id, sbm$nodes$id)
-  type_map <- attr(sbm, 'type_map')
 
   if(node_not_in_network){
 
     if(type_missing){
-      type <- type_map$type[1]
-      type_index <- type_map$type_index[1]
+      type <- sbm$nodes$type[1]
       if(show_messages){
         message(glue::glue("{id} node not in network but has no specified type. Defaulting to {type}"))
       }
     }
 
-    type_index <- type_map$type_index[type_map$type == type]
     # Add node to nodes list
-    sbm$nodes <- dplyr::bind_rows(sbm$nodes,
-                                  dplyr::tibble(id = id, type = type))
-
+    sbm$nodes <- dplyr::bind_rows(sbm$nodes, dplyr::tibble(id = id, type = type))
 
     # Before we start modifying the model object, first verify it
     sbm <- verify_model(sbm)
 
     # Add node to s4 model class
-    attr(sbm, 'model')$add_node(id, type_index, 0L)
+    attr(sbm, 'model')$add_node(id, get_type_index(type), 0L)
 
     # Update state with new nodes [inefficient]
     attr(sbm, "state") <- attr(sbm, 'model')$get_state()
@@ -88,7 +83,6 @@ add_node.sbm_network <- function(sbm, id, type = NULL, show_messages = TRUE){
                         "Replacing with type with {type} would invalidate model state.",
                         "Try rebuilding model or using different node id"))
       }
-
 
     }
 
