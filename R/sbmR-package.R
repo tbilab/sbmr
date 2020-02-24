66e854fcf4222902effea21faf5532489f9f5f76
Nick Strayer <nick.strayer@gmail.com>
2020-02-24 13:31:07 -0600

    Added basic documentation of the rcpp exported SBM class

--- R/sbmR-package.R
+++ R/sbmR-package.R
@@ -11,9 +11,19 @@
 
 Rcpp::loadModule("SBM", TRUE)
 
+#' SBM C++ class
+#'
+#' This is the R reference to the c++ class that implements SBM functionality.
+#' Almost all interfacing with this methods will be taken care of with the s3
+#' class `sbm_network` and thus this should be ignored for the vast majority of
+#' users. Documentation of all available methods is available in the c++ wrapper
+#' code using the RCPP module protocol. See \code{\link{Rcpp::loadModule}} for more info.
+#'
+setClass("Rcpp_SBM")
+
+
 # Set show method for object so we get something other than an ugly pointer to
 # object
-setClass("Rcpp_SBM")
 setMethod( "show", "Rcpp_SBM" , function(object) {
   show(object$get_state())
 })
