5d54653e0e6099c82ec3794cbd13add58df4fdab
Nick Strayer <nick.strayer@gmail.com>
2020-02-25 17:39:34 -0600

    Cleaned up the collapse_run test (still commented out)

--- tests/testthat/test-agglomerative_merging.R
+++ tests/testthat/test-agglomerative_merging.R
@@ -47,58 +47,28 @@ test_that("Requesting just the final merge step returns just the final merge ste
   expect_equal(nrow(net$collapse_results), 1)
 })
 
-#
-#
+
+
 # test_that("Collapse run works in sequential mode", {
 #   set.seed(7)
 #   library(tidyverse)
 #
-#   # net <- sim_basic_block_network(n_blocks          = 2,
-#   #                                n_nodes_per_block = 30,
-#   #                                return_edge_propensities = TRUE,
-#   #                                random_seed       = 42)
+#   net <- sim_basic_block_network(n_blocks          = 2,
+#                                  n_nodes_per_block = 30,
+#                                  return_edge_propensities = TRUE,
+#                                  random_seed       = 42)
 #
-#   block_info <- dplyr::tribble(
-#     ~block, ~n_nodes,
-#     "a",       30,
-#     "b",       30,
-#   )
-#
-#   edge_props <- tribble(
-#     ~block_1, ~block_2, ~propensity,
-#          "a",      "a",         0.5,
-#          "a",      "b",         0.2,
-#          "b",      "b",         0.001,
-#   )
-#
-#   net <- sim_sbm_network(block_info,
-#                   edge_props,
-#                   edge_dist = purrr::rbernoulli,
-#                   random_seed = 42)
-# #
-# #   net$edges %>%
-# #     mutate(i = row_number()) %>%
-# #     pivot_longer(c(from, to)) %>%
-# #     left_join(net$nodes, by = c("value" = "id")) %>%
-# #     group_by(i) %>%
-# #     summarise(pair = sort(block) %>% paste(collapse = "-")) %>%
-# #     count(pair)
-# #
-#
-#   # visualize_network(net)
-#   for(i in 1:100){
-#     new_sbm_network(edges = net$edges,
-#                     nodes = net$nodes,
-#                     random_seed = 42) %>%
-#       collapse_run(num_final_blocks = 1:5, num_mcmc_sweeps = 3)
+#   for(i in 1:4){
+#       new_sbm_network(edges = net$edges,
+#                       nodes = net$nodes,
+#                       random_seed = 42) %>%
+#         collapse_run(num_final_blocks = 1:5, num_mcmc_sweeps = 3)
 #   }
 #
 #
-#
-#
-#   expect_equal(nrow(net$collapse_results), 5)
+#   # expect_equal(nrow(net$collapse_results), 5)
 # })
-#
-#
-#
-#
+
+
+
+
