64f2d41bf619117e44dd373c9d37d09e78a90b74
Nick Strayer <nick.strayer@gmail.com>
2020-02-27 09:49:05 -0600

    Script used to find error in agglomerative merge run

---
+++ tests/manual/collapse_run_scratch.R
@@ -0,0 +1,27 @@
+# library(tidyverse)
+library(sbmR)
+
+block_info <- dplyr::tribble(
+  ~block, ~n_nodes,
+  "a",       30,
+  "b",       30,
+)
+
+edge_props <- dplyr::tribble(
+  ~block_1, ~block_2, ~propensity,
+  "a",      "a",         0.5,
+  "a",      "b",         0.2,
+  "b",      "b",         0.001,
+)
+
+
+for(i in 1:100){
+  print(i)
+  set.seed(42)
+  sim_sbm_network(block_info,
+                  edge_props,
+                  edge_dist = purrr::rbernoulli,
+                  random_seed = 42) %>%
+   collapse_run(num_final_blocks = 2:5, num_mcmc_sweeps = 3)
+}
+
