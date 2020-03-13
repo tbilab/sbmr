405b37157832b6b5ed6e32436033be2e9ba2191c
Nick Strayer <nick.strayer@gmail.com>
2020-03-13 18:57:31 -0500

    Updated sampler to be header-only and use templates to avoid dependency on Node class. Added a helpful shuffle member as well.

--- src/Sampler.h
+++ src/Sampler.h
@@ -4,30 +4,38 @@
 #ifndef __SAMPLER_INCLUDED__
 #define __SAMPLER_INCLUDED__
 
-#include "Node.h"
-
+#include "error_and_message_macros.h"
+#include <list>
+#include <vector>
 #include <random>
 
-// This is a mouthful and confusing
-using TwisterSeedMaker = std::mt19937::result_type;
+using Random_Engine = std::mt19937;
+using C_Unif        = std::uniform_real_distribution<>;
+using D_Unif        = std::uniform_int_distribution<>;
 
 //=================================
 // Main class declaration
 //=================================
 class Sampler {
+  private:
+  C_Unif gen_cont_unif;
+
   public:
+  // Attributes
+  // ===========================================================================
+  Random_Engine generator; // Generates random unsigned ints
+
   // Constructors
   // ===========================================================================
 
   // Setup with a random seed based on clock
   Sampler()
-      : generator(TwisterSeedMaker(time(0)))
   {
   }
 
   // Setup with a random seed based on passed seed
   Sampler(int seed)
-      : generator(TwisterSeedMaker(seed))
+      : generator(seed)
   {
   }
 
@@ -36,23 +44,72 @@ class Sampler {
       : generator(old_sampler.generator)
   {
   }
+
   Sampler& operator=(const Sampler&) = delete;
 
   // Move operations
   Sampler(Sampler&&) = delete;
   Sampler& operator=(Sampler&&) = delete;
 
-  // Attributes
-  // ===========================================================================
-  std::mt19937                     generator; // Generates random unsigned ints
-  std::uniform_real_distribution<> unif_gen;  // Generates random uniforms
-
   // ==========================================
   // Methods
-  double  draw_unif();                             // Return random uniform value between 0 and 1.
-  int     get_rand_int(const int max_val);        // Sample from discrete random uniform from 0 to max
-  NodePtr sample(const NodeList& nodes_to_sample); // Sample random node from a list of nodes
-  NodePtr sample(const NodeVec& nodes_to_sample);  // Sample random node from vector of nodes
+  // =============================================================================
+  // Draw a single sample from a random uniform (0 - 1] distribution
+  // =============================================================================
+  double draw_unif() { return gen_cont_unif(generator); }
+
+  // =============================================================================
+  // Draw single sample from a discrete random uniform (0 - max_val] distribution
+  // =============================================================================
+  int get_rand_int(const int max_val)
+  {
+    // Initialize a uniform random number generator with max at desired value
+    D_Unif dist(0, max_val);
+
+    return dist(generator);
+  }
+
+  // =============================================================================
+  // Sample a random element from a list
+  // =============================================================================
+  template <typename T>
+  T sample(const std::list<T>& node_list)
+  {
+    const int size_of_list = node_list.size();
+
+    if (size_of_list == 0) {
+      LOGIC_ERROR("List to sample has no elements.");
+    }
+
+    // Start an iterator at begining of list
+    auto block_it = node_list.begin();
+
+    // Select a random index to grab and advance list iterator till we've walked
+    // the desired number of steps
+    std::advance(block_it, get_rand_int(node_list.size() - 1));
+
+    return *block_it;
+  }
+
+  // =============================================================================
+  // Sample random node from vector
+  // Easier than list because we can just index to a spot
+  // =============================================================================
+  template <typename T>
+  T sample(const std::vector<T>& node_vec)
+  {
+    // Select a random index to return element at that index
+    return node_vec.at(get_rand_int(node_vec.size() - 1));
+  }
+
+  // =============================================================================
+  // Shuffle a vector in-place
+  // =============================================================================
+  template <typename T>
+  void shuffle(std::vector<T>& vec)
+  {
+    std::shuffle(vec.begin(), vec.end(), generator);
+  }
 };
 
 #endif
