dc9caf2b051cca81ece24d22ff84b1347e914a5d
Nick Strayer <nick.strayer@gmail.com>
2020-03-19 16:32:26 -0500

    Added empty test script for testing sbm algorithms

---
+++ src/cpp_tests/tests-sbm_network_algorithms.cpp
@@ -0,0 +1,9 @@
+#include "../network.h"
+#include "../swap_blocks.h"
+#include "catch.hpp"
+
+// TEST_CASE("Basic initialization of network", "[Network]")
+// {
+//   SBM_Network my_net({ "m", "n" }, 42);
+
+// }
