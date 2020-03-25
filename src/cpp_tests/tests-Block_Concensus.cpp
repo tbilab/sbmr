c4211a806782ebcfae05f273aea93ac71fdd400a
Nick Strayer <nick.strayer@gmail.com>
2020-03-25 18:30:28 -0500

    Added shell of script for testing block concensus class

---
+++ src/cpp_tests/tests-Block_Concensus.cpp
@@ -0,0 +1,11 @@
+#include "build_testing_networks.h"
+#include "../swap_blocks.h"
+#include "catch.hpp"
+
+
+TEST_CASE("Building block concensus - Simple Bipartite", "[SBM]")
+{
+
+  auto my_sbm = simple_bipartite();
+
+}
