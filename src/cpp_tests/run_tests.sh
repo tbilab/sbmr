6da37ecf101baab192d4fea42b7163be9eb4797f
Nick Strayer <nick.strayer@gmail.com>
2020-03-26 14:12:26 -0500

    Added non-activated tests for of mcmc sweep

--- src/cpp_tests/run_tests.sh
+++ src/cpp_tests/run_tests.sh
@@ -29,6 +29,8 @@ g++ -std=c++11 ${OPTIMIZATION_LEVEL} -DNO_RCPP=1\
   cpp_tests/tests-edge.cpp \
   cpp_tests/tests-sampler.cpp \
   cpp_tests/tests-sbm_network.cpp \
+  cpp_tests/tests-Block_Concensus.cpp \
+  cpp_tests/tests-mcmc_sweep.cpp \
   cpp_tests/tests-sbm_network_algorithms.cpp \
   -o cpp_tests/run_tests.o 
 
---
+++ src/cpp_tests/tests-mcmc_sweep.cpp
@@ -0,0 +1,29 @@
+#include "build_testing_networks.h"
+#include "../mcmc_sweep.h"
+#include "catch.hpp"
+
+
+TEST_CASE("Basic mcmc sweep - Simple Bipartite", "[SBM]")
+{
+  auto my_sbm = simple_bipartite();
+
+  auto consensus = Block_Consensus();
+
+  consensus.initialize(my_sbm.get_nodes_at_level(0));
+
+  // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
+  REQUIRE(consensus.size() == 12);
+}
+
+TEST_CASE("Basic mcmc sweep - Simple Unipartite", "[SBM]")
+{
+  auto my_sbm = simple_unipartite();
+
+  auto consensus = Block_Consensus();
+
+  consensus.initialize(my_sbm.get_nodes_at_level(0));
+
+  // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
+  // 6*5/2
+  REQUIRE(consensus.size() == 15);
+}
