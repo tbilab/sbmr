b522f49f4b3274142dfa4e304a58eb38d3bb795d
Nick Strayer <nick.strayer@gmail.com>
2020-03-26 18:13:01 -0500

    Finished fleshing out the mcmc sweep function

--- src/mcmc_sweep.h
+++ src/mcmc_sweep.h
@@ -1,20 +1,20 @@
 #pragma once
 
-#include "network.h"
-#include "calc_move_results.h"
 #include "Block_Consensus.h"
+#include "calc_move_results.h"
+#include "network.h"
 
 struct MCMC_Sweeps {
-    std::vector<double> sweep_entropy_delta;
-    std::vector<int> sweep_num_nodes_moved;
-    Block_Consensus block_consensus;
-    std::vector<string> nodes_moved;
-    MCMC_Sweeps(const int n)
-    {
-        // Preallocate the entropy change and num groups moved in sweep vectors and
-        sweep_entropy_delta.reserve(n);
-        sweep_num_nodes_moved.reserve(n);
-    }
+  std::vector<double> sweep_entropy_delta;
+  std::vector<int> sweep_num_nodes_moved;
+  Block_Consensus block_consensus;
+  std::vector<string> nodes_moved;
+  MCMC_Sweeps(const int n)
+  {
+    // Preallocate the entropy change and num groups moved in sweep vectors and
+    sweep_entropy_delta.reserve(n);
+    sweep_num_nodes_moved.reserve(n);
+  }
 };
 
 // =============================================================================
@@ -37,7 +37,7 @@ MCMC_Sweeps mcmc_sweep(SBM_Network& net,
   if (track_pairs) {
     results.block_consensus.initialize(net.get_nodes_at_level(level));
   }
-  
+
   // Check if we have any blocks ready in the network...
   const bool no_blocks_present = net.num_levels() > block_level + 1;
 
@@ -70,10 +70,9 @@ MCMC_Sweeps mcmc_sweep(SBM_Network& net,
   auto nodes = net.get_flat_level(level);
 
   for (int i = 0; i < num_sweeps; i++) {
-    OUT_MSG << "Sweep " << i << std::endl;
     // Book keeper variables for this sweeps stats
-    int    num_nodes_moved = 0;
-    double entropy_delta   = 0;
+    int num_nodes_moved  = 0;
+    double entropy_delta = 0;
 
     // Shuffle order of nodes to be run through for sweep
     net.sampler.shuffle(nodes);
@@ -84,22 +83,16 @@ MCMC_Sweeps mcmc_sweep(SBM_Network& net,
     int steps_taken = 0;
     // Loop through each node
     for (const auto& curr_node : nodes) {
-      // Check if we're running sweep with variable block numbers. If we are, we
-      // need to make sure we don't have any extra unoccupied blocks floating around,
-      // then we need to add a new block as a potential for the node to enter
-      // if (variable_num_blocks) {
-      //   clean_empty_blocks();
-      //   create_block_node(curr_node->type, block_level);
-      // }
-
       // Get a move proposal
       Node* proposed_new_block = net.propose_move(curr_node, eps);
 
-      // // If the proposed block is the nodes current block, we don't need to waste
-      // // time checking because decision will always result in same state.
-      // if (curr_node->parent == proposed_new_block) {
-      //   continue;
-      // }
+      Node* old_block = curr_node->parent();
+
+      // If the proposed block is the nodes current block, we don't need to waste
+      // time checking because decision will always result in same state.
+      if (old_block == proposed_new_block) {
+        continue;
+      }
 
       if (verbose) {
         OUT_MSG << i
@@ -124,8 +117,8 @@ MCMC_Sweeps mcmc_sweep(SBM_Network& net,
 
       // Is the move accepted?
       if (move_accepted) {
-        Node* old_block = curr_node->parent();
 
+        bool remove_empty_block = variable_num_blocks;
         if (variable_num_blocks) {
           // If the old block will still have children after the move and
           // the new block is empty block, this move will cause there to be no
@@ -133,12 +126,19 @@ MCMC_Sweeps mcmc_sweep(SBM_Network& net,
           const bool old_wont_be_empty = old_block->num_children() > 1;
           const bool new_is_empty      = proposed_new_block->num_children() == 0;
 
-          if (new_is_empty & old_wont_be_empty) {
-            net.add_block_node(curr_node->type(), block_level);
+          if (new_is_empty) {
+            if (old_wont_be_empty) {
+              // Need to add a new block node as we wont have any empty ones left
+              net.add_block_node(curr_node->type(), block_level);
+            } else {
+              // In this case the old block will become to the empty block so we
+              // don't want it to be removed by the swap_blocks function
+              remove_empty_block = false;
+            }
           }
         }
 
-        net.swap_blocks(curr_node, proposed_new_block, variable_num_blocks);
+        net.swap_blocks(curr_node, proposed_new_block, remove_empty_block);
 
         // Update results
         results.nodes_moved.push_back(curr_node->id());
@@ -161,26 +161,24 @@ MCMC_Sweeps mcmc_sweep(SBM_Network& net,
       }
     } // End current sweep
 
-    // // Update results for this sweep
-    // results.sweep_num_nodes_moved.push_back(num_nodes_moved);
-    // results.sweep_entropy_delta.push_back(entropy_delta);
+    // Update results for this sweep
+    results.sweep_num_nodes_moved.push_back(num_nodes_moved);
+    results.sweep_entropy_delta.push_back(entropy_delta);
 
-    // // Update the concensus pairs map with results if needed.
-    // if (track_pairs) {
-    //   results.block_consensus.update_pair_tracking_map(pair_moves);
-    // }
-    // ALLOW_USER_BREAKOUT; // Let R used break out of loop if need be
-  }                      // End multi-sweep loop
+    // Update the concensus pairs map with results if needed.
+    if (track_pairs) {
+      results.block_consensus.update_pair_tracking_map(pair_moves);
+    }
+    ALLOW_USER_BREAKOUT; // Let R used break out of loop if need be
+  } // End multi-sweep loop
 
   if (variable_num_blocks) {
-    OUT_MSG << "Cleaning up after end of MCMC sweep..." << std::endl;
     // Cleanup the single empty block for each type
     for (const auto& blocks_of_type : net.get_nodes_at_level(block_level)) {
       for (const auto& block : blocks_of_type) {
         if (block->num_children() == 0) {
-          OUT_MSG << "  Removed block from type " << block->type() << std::endl;
           net.delete_node(block);
-          break;
+          break; // No need to continue so break out (also would cause problems because we just modified the vector we're looping over)
         }
       }
     }
