870ebe51de7b0ddb4d2163f367ce3874097daf17
Nick Strayer <nick.strayer@gmail.com>
2020-03-26 18:14:05 -0500

    Added call to mcmc sweep function for bipartite test

--- src/cpp_tests/tests-mcmc_sweep.cpp
+++ src/cpp_tests/tests-mcmc_sweep.cpp
@@ -7,23 +7,30 @@ TEST_CASE("Basic mcmc sweep - Simple Bipartite", "[SBM]")
 {
   auto my_sbm = simple_bipartite();
 
-  auto consensus = Block_Consensus();
+  auto sweep_res = mcmc_sweep(my_sbm,
+                              2,
+                              0.2,   // eps
+                              true, //variable num blocks
+                              false, //track pairs
+                              0,     // level
+                              true); //verbose
 
-  consensus.initialize(my_sbm.get_nodes_at_level(0));
+ 
+  // consensus.initialize(my_sbm.get_nodes_at_level(0));
 
-  // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
-  REQUIRE(consensus.size() == 12);
+  // // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
+  // REQUIRE(consensus.size() == 12);
 }
 
-TEST_CASE("Basic mcmc sweep - Simple Unipartite", "[SBM]")
-{
-  auto my_sbm = simple_unipartite();
+// TEST_CASE("Basic mcmc sweep - Simple Unipartite", "[SBM]")
+// {
+//   auto my_sbm = simple_unipartite();
 
-  auto consensus = Block_Consensus();
+//   auto consensus = Block_Consensus();
 
-  consensus.initialize(my_sbm.get_nodes_at_level(0));
+//   consensus.initialize(my_sbm.get_nodes_at_level(0));
 
-  // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
-  // 6*5/2
-  REQUIRE(consensus.size() == 15);
-}
+//   // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
+//   // 6*5/2
+//   REQUIRE(consensus.size() == 15);
+// }
