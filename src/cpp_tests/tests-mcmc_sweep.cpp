6c84e1f8463bb0f1fc126424a671444d17a377ae
Nick Strayer <nick.strayer@gmail.com>
2020-03-27 11:30:14 -0500

    Re-enabled the basic mcmc sweep tests for unipartite network.

--- src/cpp_tests/tests-mcmc_sweep.cpp
+++ src/cpp_tests/tests-mcmc_sweep.cpp
@@ -13,24 +13,20 @@ TEST_CASE("Basic mcmc sweep - Simple Bipartite", "[SBM]")
                               true, //variable num blocks
                               false, //track pairs
                               0,     // level
-                              true); //verbose
+                              false); //verbose
 
- 
-  // consensus.initialize(my_sbm.get_nodes_at_level(0));
-
-  // // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
-  // REQUIRE(consensus.size() == 12);
 }
 
-// TEST_CASE("Basic mcmc sweep - Simple Unipartite", "[SBM]")
-// {
-//   auto my_sbm = simple_unipartite();
+TEST_CASE("Basic mcmc sweep - Simple Unipartite", "[SBM]")
+{
+  auto my_sbm = simple_unipartite();
 
-//   auto consensus = Block_Consensus();
-
-//   consensus.initialize(my_sbm.get_nodes_at_level(0));
-
-//   // There should be nt(nt-1)/2 pairs for each type t with nt nodes. 
-//   // 6*5/2
-//   REQUIRE(consensus.size() == 15);
-// }
+  
+  auto sweep_res = mcmc_sweep(my_sbm,
+                              2,
+                              0.2,   // eps
+                              true, //variable num blocks
+                              false, //track pairs
+                              0,     // level
+                              false); //verbose
+}
