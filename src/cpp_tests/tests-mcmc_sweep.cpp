d605599618b28726f8dc7558c64e1cdb18da8e60
Nick Strayer <nick.strayer@gmail.com>
2020-03-27 11:34:12 -0500

    Added simple tests to make sure the sweep results had the correct number of entries for the number of sweeps requested

--- src/cpp_tests/tests-mcmc_sweep.cpp
+++ src/cpp_tests/tests-mcmc_sweep.cpp
@@ -7,26 +7,35 @@ TEST_CASE("Basic mcmc sweep - Simple Bipartite", "[SBM]")
 {
   auto my_sbm = simple_bipartite();
 
+  const int n_sweeps = 2;
+
   auto sweep_res = mcmc_sweep(my_sbm,
-                              2,
+                              n_sweeps,
                               0.2,   // eps
                               true, //variable num blocks
                               false, //track pairs
                               0,     // level
                               false); //verbose
 
+  REQUIRE(sweep_res.sweep_entropy_delta.size() == n_sweeps);
+  REQUIRE(sweep_res.sweep_num_nodes_moved.size() == n_sweeps);
 }
 
 TEST_CASE("Basic mcmc sweep - Simple Unipartite", "[SBM]")
 {
+  const int n_sweeps = 2;
+
   auto my_sbm = simple_unipartite();
 
   
   auto sweep_res = mcmc_sweep(my_sbm,
-                              2,
+                              n_sweeps,
                               0.2,   // eps
                               true, //variable num blocks
                               false, //track pairs
                               0,     // level
                               false); //verbose
+
+  REQUIRE(sweep_res.sweep_entropy_delta.size() == n_sweeps);
+  REQUIRE(sweep_res.sweep_num_nodes_moved.size() == n_sweeps);
 }
