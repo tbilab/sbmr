4356fc6941122655096d7df311b753fac0f100de
Nick Strayer <nick.strayer@gmail.com>
2020-03-30 13:55:49 -0500

    Reworked back to not explicitely using pointers in ordered pair because they werent neccesary and also renamed back to just ordered_pair. I have commitment issues.

--- src/Ordered_Pair.h
+++ src/Ordered_Pair.h
@@ -3,19 +3,19 @@
 #include <unordered_set>
 
 template <typename T>
-class Ordered_Ptr_Pair {
+class Ordered_Pair {
   private:
-  T* val_1;
-  T* val_2;
+  T val_1;
+  T val_2;
 
   public:
-  Ordered_Ptr_Pair(const T* a, const T* b)
+  Ordered_Pair(const T a, const T b)
       : val_1(a < b ? a : b)
       , val_2(a < b ? b : a)
   {
   }
 
-  std::pair<T*, T*> get() const { return std::make_pair(val_1, val_2); }
+  std::pair<T, T> get() const { return std::make_pair(val_1, val_2); }
 
   T first() const { return val_1; }
   T second() const { return val_2; }
@@ -24,13 +24,13 @@ class Ordered_Ptr_Pair {
 };
 
 template <typename T>
-bool operator==(const Ordered_Ptr_Pair<T>& a, const Ordered_Ptr_Pair<T>& b)
+bool operator==(const Ordered_Pair<T>& a, const Ordered_Pair<T>& b)
 {
   return a.first() == b.first() && a.second() == b.second();
 }
 
 template <typename T>
-bool operator<(const Ordered_Ptr_Pair<T>& a, const Ordered_Ptr_Pair<T>& b)
+bool operator<(const Ordered_Pair<T>& a, const Ordered_Pair<T>& b)
 {
   // If the first value is same as second, then check the second
   if (a.first() == b.first()) {
@@ -43,12 +43,12 @@ bool operator<(const Ordered_Ptr_Pair<T>& a, const Ordered_Ptr_Pair<T>& b)
 // Hash function for ordered pairs so they can be used in hashed containers like
 // unordered_map and unordered_set
 template <typename T>
-struct Ordered_Ptr_Pair_Hash {
-  size_t operator()(const Ordered_Ptr_Pair<T>& p) const
+struct Ordered_Pair_Hash {
+  size_t operator()(const Ordered_Pair<T>& p) const
   {
-    return std::hash<T*>()(p.first()) ^ std::hash<T*>()(p.second());
+    return std::hash<T>()(p.first()) ^ std::hash<T>()(p.second());
   }
 };
 
 template <typename T>
-using Ordered_Ptr_Pair_Set = std::unordered_set<Ordered_Ptr_Pair<T>, Ordered_Ptr_Pair_Hash<T>>;
+using Ordered_Pair_Set = std::unordered_set<Ordered_Pair<T>, Ordered_Pair_Hash<T>>;
