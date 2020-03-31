c079a6a2600ff10a8dec536488b3de51bf007f7b
Nick Strayer <nick.strayer@gmail.com>
2020-03-31 16:52:20 -0500

    Added a multi-merge run for the unipartite test

--- src/cpp_tests/tests-agglomerative_merge.cpp
+++ src/cpp_tests/tests-agglomerative_merge.cpp
@@ -46,12 +46,12 @@ TEST_CASE("Agglomerative merge steps - Simple Bipartite", "[SBM]")
 
 TEST_CASE("Agglomerative merge steps - Simple Unipartite", "[SBM]")
 {
+  const int num_initial_blocks = 4;
 
   auto my_sbm = simple_unipartite();
-
-  const int num_initial_blocks = my_sbm.num_nodes_at_level(1);
-
-  REQUIRE(num_initial_blocks == 3);
+  // We need to erase the original block stucture and give every node its own block to have enough blocks for this
+  my_sbm.remove_blocks();
+  my_sbm.initialize_blocks(num_initial_blocks);
 
   // Run aglomerative merge with best single merge done
   const auto single_merge = agglomerative_merge(my_sbm,
@@ -68,18 +68,20 @@ TEST_CASE("Agglomerative merge steps - Simple Unipartite", "[SBM]")
   // Make sure entropy has gone up as we would expect
   REQUIRE(single_merge.entropy_delta > 0);
 
-  // // Run again but this time merging the best 2
-  // auto new_sbm = simple_bipartite();
+  // Run again but this time merging the best 2
+  auto new_sbm = simple_unipartite();
+  new_sbm.remove_blocks();
+  new_sbm.initialize_blocks(num_initial_blocks);
 
-  // const auto double_merge = agglomerative_merge(new_sbm,
-  //                                               1,    // block_level,
-  //                                               2,    // num_merges_to_make,
-  //                                               5,    // num_checks_per_block,
-  //                                               0.1); // eps
+  const auto double_merge = agglomerative_merge(new_sbm,
+                                                1,    // block_level,
+                                                2,    // num_merges_to_make,
+                                                5,    // num_checks_per_block,
+                                                0.1); // eps
 
-  // // Make sure that we now have two fewer blocks per type than before
-  // REQUIRE(2 == num_initial_blocks - new_sbm.num_nodes_at_level(1));
+  // Make sure that we now have two fewer blocks per type than before
+  REQUIRE(2 == num_initial_blocks - new_sbm.num_nodes_at_level(1));
 
-  // // Entropy should up even more with two merges
-  // REQUIRE(single_merge.entropy_delta < double_merge.entropy_delta);
+  // Entropy should up even more with two merges
+  REQUIRE(single_merge.entropy_delta < double_merge.entropy_delta);
 }
