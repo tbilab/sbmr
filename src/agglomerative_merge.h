c85a956083797ee42ca46e0d431c6756b261da03
Nick Strayer <nick.strayer@gmail.com>
2020-03-31 16:25:53 -0500

    Agglomerative merging now doesn't use metablocks at all and instead works right at the block level. This saves lots of computational overhead and also makes the code easier to reason about

--- src/agglomerative_merge.h
+++ src/agglomerative_merge.h
@@ -28,52 +28,54 @@ using Node_Set        = std::unordered_set<Node*>;
 using Node_Pair       = Ordered_Pair<Node*>;
 using Best_Move_Queue = std::priority_queue<std::pair<double, Node_Pair>>;
 
-inline double merge_entropy_delta(const Node* absorbing_block, const Node* absorbed_block)
+inline double merge_entropy_delta(const Node_Pair& merge_pair)
 {
+  // Block b will be absorbed into block a
+  const Node* block_a      = merge_pair.first();
+  const Node* block_b      = merge_pair.second();
+  const int block_a_degree = block_a->degree();
+  const int block_b_degree = block_b->degree();
+  const int merged_degree  = block_a_degree + block_b_degree;
 
-  const int block_level            = absorbing_block->level();
-  const int absorbing_block_degree = absorbing_block->degree();
-  const int absorbed_block_degree  = absorbed_block->degree();
-  const int merged_degree          = absorbing_block_degree + absorbed_block_degree;
+  Edge_Count_Map block_a_neighbor_counts = block_a->gather_neighbors_at_level(block_a->level());
+  Edge_Count_Map block_b_neighbor_counts = block_b->gather_neighbors_at_level(block_a->level());
 
-  Edge_Count_Map absorbing_block_neighbor_counts = absorbing_block->gather_neighbors_at_level(block_level);
-  Edge_Count_Map absorbed_block_neighbor_counts  = absorbed_block->gather_neighbors_at_level(block_level);
+  // Entropy accumulation will happen into this variable.
+  double merge_entropy_delta = 0.0;
 
-  // First get partial entropy from both blocks pre-merger
-  double pre_merge_entropy = 0.0;
-  for (const auto& a_to_t : absorbing_block_neighbor_counts) {
+  // First we will add in entropy from before merger
+  for (const auto& a_to_t : block_a_neighbor_counts) {
     const Node* block_t = a_to_t.first;
-    const double scalar = block_t == absorbing_block ? 2 : 1;
-    pre_merge_entropy += ent(a_to_t.second, absorbing_block_degree, block_t->degree()) / scalar;
+    const double scalar = block_t == block_a ? 2 : 1;
+    merge_entropy_delta += ent(a_to_t.second, block_a_degree, block_t->degree()) / scalar;
   }
-  for (const auto& b_to_t : absorbed_block_neighbor_counts) {
+  for (const auto& b_to_t : block_b_neighbor_counts) {
     const Node* block_t = b_to_t.first;
     // Merge edge counts into absorbing blocks counts
-    absorbing_block_neighbor_counts[block_t] += b_to_t.second;
+    block_a_neighbor_counts[block_t] += b_to_t.second;
 
-    if (block_t == absorbing_block) continue; // Avoid double counting the a-b pair
-    const double scalar = block_t == absorbed_block ? 2 : 1;
-    pre_merge_entropy += ent(b_to_t.second, absorbed_block_degree, block_t->degree()) / scalar;
+    if (block_t == block_a) continue; // Avoid double counting the a-b pair
+    const double scalar = block_t == block_b ? 2 : 1;
+    merge_entropy_delta += ent(b_to_t.second, block_b_degree, block_t->degree()) / scalar;
   }
 
   // Now merge the counts to absorbed block into the absorbing block and get rid of absorbed entry
-  // if the pair exists
-  const auto absorbing_to_absorbed_it = absorbing_block_neighbor_counts.find(absorbed_block);
-  if (absorbing_to_absorbed_it != absorbing_block_neighbor_counts.end()) {
-    absorbing_block_neighbor_counts[absorbing_block] += absorbing_block_neighbor_counts[absorbed_block];
-    absorbing_block_neighbor_counts.erase(absorbed_block);
+  // if the pair exists. This only happens if we're in a unipartite network
+  const auto a_to_b_it = block_a_neighbor_counts.find(block_b);
+  if (a_to_b_it != block_a_neighbor_counts.end()) {
+    block_a_neighbor_counts[block_a] += a_to_b_it->second;
+    block_a_neighbor_counts.erase(a_to_b_it);
   }
 
-  double post_merge_entropy = 0.0;
-  for (const auto& a_to_t : absorbing_block_neighbor_counts) {
+  for (const auto& a_to_t : block_a_neighbor_counts) {
     const Node* block_t     = a_to_t.first;
-    const bool is_absorbing = block_t == absorbing_block;
+    const bool is_absorbing = block_t == block_a;
     const int t_degree      = is_absorbing ? merged_degree : block_t->degree();
     const double scalar     = is_absorbing ? 2 : 1;
-    post_merge_entropy += ent(a_to_t.second, merged_degree, t_degree) / scalar;
+    merge_entropy_delta -= ent(a_to_t.second, merged_degree, t_degree) / scalar;
   }
 
-  return pre_merge_entropy - post_merge_entropy;
+  return merge_entropy_delta;
 }
 
 // =============================================================================
@@ -86,11 +88,6 @@ inline Merge_Step agglomerative_merge(SBM_Network& net,
                                       const double& eps,
                                       const bool allow_exhaustive = true)
 {
-  // Strip away any previous meta-block level if it existed and
-  // build a new level of meta-blocks with one metablock per block
-  net.remove_higher_levels(block_level);
-  net.initialize_blocks();
-
   // Set to keep track of the attepted merge pairs
   auto checked_pairs = Ordered_Pair_Set<Node*>();
 
@@ -98,10 +95,9 @@ inline Merge_Step agglomerative_merge(SBM_Network& net,
   Best_Move_Queue best_merges;
 
   for (int type = 0; type < net.num_types(); type++) {
+    const auto& blocks_of_type = net.get_nodes_of_type(type, block_level);
+    const int n_blocks_of_type = blocks_of_type.size();
 
-    const auto& blocks_of_type     = net.get_nodes_of_type(type, block_level);
-    const int n_blocks_of_type     = blocks_of_type.size();
-    const int n_neighbors_for_type = net.num_possible_neighbors(type, block_level);
     // Compare how many checks will be done for merges. If this number is greater than just an exhaustive
     // search of all possible pairs, and the user allows it, just perform an exhaustive search of mergers.
     // The exhaustive search also benefits from not having to do the move proposal step.
@@ -114,37 +110,33 @@ inline Merge_Step agglomerative_merge(SBM_Network& net,
       for (int i = 0; i < n_blocks_of_type; i++) {
         const auto block_i = blocks_of_type.at(i).get();
         for (int j = i + 1; j < n_blocks_of_type; j++) {
-          const auto block_j = blocks_of_type.at(j).get();
+          const auto merge_pair = Node_Pair(block_i, blocks_of_type.at(j).get());
 
-          const auto merge_delta = merge_entropy_delta(block_i, block_j);
-
-          best_merges.push(std::make_pair(-merge_delta, Node_Pair(block_i, block_j)));
+          best_merges.push(std::make_pair(-merge_entropy_delta(merge_pair), merge_pair));
         }
       }
     } else {
       // Loop through each block and propose m moves using move proposal function
       for (const auto& block : blocks_of_type) {
-        const Node* metablock = block->parent();
+        const auto block_i = block.get();
 
         for (int i = 0; i < num_checks_per_block; i++) {
-          const Node* proposed_metablock = net.propose_move(block.get(), eps);
-          const auto merge_pair          = Node_Pair(block.get(), proposed_metablock->get_only_child());
+          Node* block_j = net.propose_merge(block_i, eps);
 
-          // Ignore if proposed metablock is just current metablock
-          if (metablock == proposed_metablock) continue;
+          // Ignore if proposal if it's just the block itself
+          if (block_i == block_j) continue;
+
+          const auto merge_pair = Node_Pair(block_i, block_j);
 
           // See if this combo of groups has already been looked at
           const bool pair_already_checked = !checked_pairs.insert(merge_pair).second;
           if (pair_already_checked) continue;
 
-          const auto merge_delta = merge_entropy_delta(block.get(), proposed_metablock->get_only_child());
-
           // Calculate entropy delta for merge and place into results queue.
-          best_merges.push(std::make_pair(-merge_delta, merge_pair));
+          best_merges.push(std::make_pair(-merge_entropy_delta(merge_pair), merge_pair));
         } // End of m merge checks
       }   // End of loop over nodes of a type
     }
-
   } // End of loop over types in level
 
   // Now we find the top merges...
@@ -179,9 +171,6 @@ inline Merge_Step agglomerative_merge(SBM_Network& net,
     // here because we negated the entropy delta when inserting into the queue
     results.entropy_delta -= best_merge.first;
   }
-
-  // Dump the highest level of blocks before making merges
-  net.remove_last_level();
 
   for (const auto& merge_pair : top_distinct_merges) {
     net.merge_blocks(merge_pair.first(), merge_pair.second());
