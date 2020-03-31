2b154e3b28fde1d954e77a583fdb86cead6235e4
Nick Strayer <nick.strayer@gmail.com>
2020-03-31 09:48:45 -0500

    Updated tests to use the new remove_last_level method name

--- src/cpp_tests/tests-sbm_network.cpp
+++ src/cpp_tests/tests-sbm_network.cpp
@@ -168,17 +168,17 @@ TEST_CASE("Metablock initialization", "[Network]")
   REQUIRE(my_net.num_nodes_at_level(2) == 4);
 
   // Now remove the metablocks
-  my_net.delete_block_level();
+  my_net.remove_last_level();
 
   // Should be back to two levels
   REQUIRE(my_net.num_levels() == 2);
 
   // Remove the blocks
-  my_net.delete_block_level();
+  my_net.remove_last_level();
   REQUIRE(my_net.num_levels() == 1);
 
   // Cant remove blocks when no levels are left
-  REQUIRE_THROWS(my_net.delete_block_level());
+  REQUIRE_THROWS(my_net.remove_last_level());
 }
 
 TEST_CASE("Swapping of blocks", "[Network]")
