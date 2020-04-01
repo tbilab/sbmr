53849cc0ba54091b83ee892950346c6b1d4479f7
Nick Strayer <nick.strayer@gmail.com>
2020-04-01 15:59:28 -0500

    Updated debugger to run collapse_blocks function

--- src/debugging/debug.cpp
+++ src/debugging/debug.cpp
@@ -1,6 +1,6 @@
 // This script serves as a portal into debugging. I use it by pasting in any
 // tests that are failing and then stepping through to see what's going on.
-#include "../agglomerative_merge.h"
+#include "../collapse_blocks.h"
 #include "../cpp_tests/build_testing_networks.h"
 #include "../network.h"
 
@@ -8,17 +8,17 @@
 
 int main(int argc, char** argv)
 {
-  // Setup simple SBM model
-  auto my_sbm = simple_unipartite();
+  auto my_sbm = simple_bipartite();
 
-  const int num_initial_blocks = my_sbm.num_nodes_at_level(1);
-
-  // Run aglomerative merge with best single merge done
-  const auto single_merge = agglomerative_merge(my_sbm,
-                                                1,    // block_level,
-                                                1,    // num_merges_to_make,
-                                                5,    // num_checks_per_block,
-                                                0.1); // eps
+  auto collapse_to_2_res = collapse_blocks(my_sbm,
+                                           0,     // node_level,
+                                           2,     // B_end,
+                                           5,     // n_checks_per_block,
+                                           0,     // n_mcmc_sweeps,
+                                           1.1,   // sigma,
+                                           0.01,  // eps,
+                                           true,  // report all steps,
+                                           true); // Allow exhaustive
 
   return 0;
 }
