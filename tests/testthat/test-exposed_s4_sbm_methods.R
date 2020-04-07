a22b7c8dcf818bd8f3a3273cd6a936f0b720c1c0
Nick Strayer <nick.strayer@gmail.com>
2020-04-07 16:21:05 -0500

    Fleshed out the tests of the collapse_blocks method

--- tests/testthat/test-exposed_s4_sbm_methods.R
+++ tests/testthat/test-exposed_s4_sbm_methods.R
@@ -207,9 +207,10 @@ test_that("Agglomerative merging", {
   sbm <- new_sbm_network(edges, edges_from_column = a_node, edges_to_column = b_node, bipartite_edges = TRUE) %>%
     attr("model")
 
+  final_num_blocks <- 2
   # Run block collapsing with one block per merge
   collapse_results <- sbm$collapse_blocks(0,    # node_level,
-                                          2,    # B_end,
+                                          final_num_blocks,    # B_end,
                                           3,    # n_checks_per_block,
                                           0,    # n_mcmc_sweeps,
                                           0.9,  # sigma,
@@ -217,10 +218,22 @@ test_that("Agglomerative merging", {
                                           TRUE, # report_all_steps = true,
                                           TRUE) # allow_exhaustive = true
 
+  # Reporting all steps should, return as many list elements as there are possible steps
+  expect_equal(length(collapse_results),
+               sbm$num_nodes_at_level(0) - final_num_blocks)
 
+  collapse_results_just_final <- sbm$collapse_blocks(0,    # node_level,
+                                          final_num_blocks,    # B_end,
+                                          3,    # n_checks_per_block,
+                                          0,    # n_mcmc_sweeps,
+                                          0.9,  # sigma,
+                                          0.01, # eps,
+                                          FALSE, # report_all_steps = true,
+                                          TRUE) # allow_exhaustive = true
 
-
-
+  # Just reporting the final step should... just report the final step
+  expect_equal(names(collapse_results_just_final),
+               c("entropy_delta", "state", "num_blocks"))
 
 })
 
