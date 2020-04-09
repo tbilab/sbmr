c0679b442760f4b52b0fc44166107417515ab890
Nick Strayer <nick.strayer@gmail.com>
2020-04-09 17:26:18 -0500

    Updated the entropy calculation s3 test to work with the new api

--- tests/testthat/test-calculating_entropy.R
+++ tests/testthat/test-calculating_entropy.R
@@ -1,31 +1,34 @@
-# test_that("computing entropy", {
-#   # Start with nodes 3 nodes attached to 2 blocks
-#   net <- sim_random_network(n_nodes = 25,
-#                             prob_of_edge = 0.5,
-#                             random_seed = 42) %>%
-#     initialize_blocks(10)
-#
-#   first_entropy <- net %>% get_entropy()
-#
-#   start_state <- get_state(net)
-#
-#   # Choose random node
-#   random_node <- start_state %>%
-#     dplyr::filter(level == 0) %>%
-#     dplyr::sample_n(1)
-#
-#   # Find a _new_ parent for our random node
-#   random_new_parent <- start_state %>%
-#     dplyr::filter(level == 1, id != random_node$parent) %>%
-#     dplyr::sample_n(1)
-#
-#   # # Assign node its new parent
-#   # net <- net %>% set_node_parent(child_id = random_node$id, parent_id = random_new_parent$id)
-#   #
-#   # # Record entropy again
-#   # second_entropy <- net %>% get_entropy()
-#   #
-#   # # Entropy should have changed
-#   # expect_false(first_entropy == second_entropy)
-# })
-#
+test_that("computing entropy", {
+  # Start with nodes 3 nodes attached to 2 blocks
+  net <- sim_random_network(n_nodes = 25,
+                            prob_of_edge = 0.5,
+                            random_seed = 42) %>%
+    initialize_blocks(10)
+
+  first_entropy <- net %>% get_entropy()
+
+  start_state <- get_state(net)
+
+  # Choose random node
+  random_node <- start_state %>%
+    dplyr::filter(level == 0) %>%
+    dplyr::sample_n(1)
+
+  # Find a _new_ parent for our random node
+  random_new_parent <- start_state %>%
+    dplyr::filter(parent != random_node$parent) %>%
+    dplyr::sample_n(1) %>%
+    dplyr::pull(parent)
+
+  # Assign node its new parent by modifying state
+  net <- net %>%
+    update_state(dplyr::mutate(start_state,
+                              parent = ifelse(id == random_node$id, random_new_parent, parent)))
+
+  # Record entropy again
+  second_entropy <- net %>% get_entropy()
+
+  # Entropy should have changed
+  expect_false(first_entropy == second_entropy)
+})
+
