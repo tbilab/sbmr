8029ec13bd930afcc283505312da1378a91a9062
Nick Strayer <nick.strayer@gmail.com>
2020-04-09 19:12:24 -0500

    Scratch now for collapsing functions

--- tests/manual/scratch.R
+++ tests/manual/scratch.R
@@ -2,19 +2,20 @@ set.seed(42)
 
 # Start with a random network of two blocks with 25 nodes each
 net <- sim_basic_block_network(n_blocks = 3, n_nodes_per_block = 25) %>%
-  collapse_blocks(sigma = 1.4)
+  collapse_run(num_final_blocks = 1:5, sigma = 1.5)
 
-# Choose best result with default heuristic
+# # Collapse runs can be done in parallel
+# \dontrun{
+#   net <- sim_basic_block_network(n_blocks = 3, n_nodes_per_block = 25) %>%
+#     collapse_run(num_final_blocks = 1:5, sigma = 1.5, parallel = TRUE)
+# }
+
+# We can look directly at the collapse results
+net %>% get_collapse_results()
+
+# We can visualize the collapse results
+net %>% visualize_collapse_results()
+net %>% visualize_collapse_results(heuristic = "delta_ratio")
+
+# We can choose best result with default heuristic
 net <- choose_best_collapse_state(net, verbose = TRUE)
-
-# Score heuristic that fits a nonlinear model to observed values and chooses by
-# largest negative residual
-nls_score <- function(e, k){
-  entropy_model <- nls(e ~ a + b * log(k), start = list(a = max(e), b = -25))
-  -residuals(entropy_model)
-}
-
-# Choose result using custom heuristic function
-my_sbm <- choose_best_collapse_state(net,
-                                     heuristic = nls_score,
-                                     verbose = TRUE)
